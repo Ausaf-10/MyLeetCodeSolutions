fa228fd8e1e4f60397b8c01b1d99d93dc57f31de
Ausaf-10 <102360335+Ausaf-10@users.noreply.github.com>
2023-07-01 12:15:34 +0530

    Time: 120 ms (52.82%), Space: 43.9 MB (23.94%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    // Optimal Solution
+    int sumSubarrayMins(vector<int>& arr) {
+        long n = arr.size();
+        stack<pair<int,int>> s1,s2;
+        vector<int> prevSmaller(n),nextSmaller(n);
 
-int sumSubarrayMins(vector<int> &arr){
-    long n = arr.size();
-    vector<int>left(n), right(n);
-    stack<pair<int,int>>s1,s2;
-    for (int i = 0; i < n; i++){
-        int count = 1;
-        while(!s1.empty() && s1.top().first > arr[i]){
-            count += s1.top().second;
-            s1.pop();
+        // nextSmaller and prevSmaller are storing g1 and g2
+
+        // CALCULATING prevSMALLER OF EVERY ELEMENT
+        for (int i=0; i<n; i++){
+            int cnt = 1;
+            while (!s1.empty() && s1.top().first > arr[i]){
+                cnt+=s1.top().second;
+                s1.pop();
+            }
+            s1.push({arr[i],cnt});
+            prevSmaller[i] = cnt;
+        } 
+
+        //CALCULATING THE NUMBER OF NEXSTMALLER 
+        for (int i=n-1; i>=0; i--){
+            int cnt = 1;
+            while (!s2.empty() && s2.top().first >= arr[i]){
+                cnt+= s2.top().second;
+                s2.pop();
+            }
+            s2.push({arr[i],cnt});
+            nextSmaller[i] = cnt;
+        } 
+        long res = 0;
+        long mod = 1e9+7;
+        for (int i=0; i<n; i++){
+            res=(res+(long)arr[i]*nextSmaller[i]*prevSmaller[i])%mod;
         }
-        s1.push({arr[i], count});
-        left[i] = count;
-    }
-    for (int i = n-1; i >= 0; i--){
-        int count = 1;
-        while(!s2.empty() && s2.top().first >= arr[i]){
-            count += s2.top().second;
-            s2.pop();
-        }
-        s2.push({arr[i], count});
-        right[i] = count;
-    }
-
-    long res = 0, mod = 1e9 + 7;
-    for (int i = 0; i < n; i++)
-        res = (res + (long)arr[i]*left[i]*right[i])%mod;
-
-    return res;
-
-
+        return res;
 
     }
 };
