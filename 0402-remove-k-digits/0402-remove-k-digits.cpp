08fae928289795646e5a5c4c1d9bffc30b810984
Ausaf-10 <102360335+Ausaf-10@users.noreply.github.com>
2023-07-03 11:45:23 +0530

    Time: 4 ms (98.68%), Space: 8.5 MB (86.5%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
+        // JUST USING STRING IN THE FORM OF A STACK SO THAT WE JUST RETURN THE ENTIRE STRING ONLY
         string ans = "";
-        if (k == 0) return num;
         int n = num.size();
+        
         if (k >= n) return "0";
 
         for(int i=0; i<n; i++){
@@ -11,7 +12,7 @@ public:
                 ans.pop_back();
                 k--;
             }
-            if (ans.size()!=0 || num[i]!='0') ans.push_back(num[i]);
+            if (ans.size()!=0 || num[i]!='0') ans.push_back(num[i]); // to avoid adding prefix 0's
         }
         
         // THIS CASE IS USED TO DELETE THE ELEMENTS TILL THE VALUE OF K
