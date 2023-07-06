dc4a19ef7a0db27c01c9114aba931d6dcf9ac937
Ausaf-10 <102360335+Ausaf-10@users.noreply.github.com>
2023-07-06 15:03:28 +0530

    Time: 125 ms (23.54%), Space: 34.1 MB (18.83%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>> maxHeap;
+
+        for (int i=0; i<arr.size(); i++){
+            int diff = abs(x-arr[i]);
+            maxHeap.push({diff,arr[i]});
+            if (maxHeap.size() > k) maxHeap.pop();
+        }
+
+        vector<int> ans;
+
+        while (!maxHeap.empty()){
+            ans.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+
+        sort(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
