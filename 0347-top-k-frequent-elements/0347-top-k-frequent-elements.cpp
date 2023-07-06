b9a58a2ae13ddf0da45ae178a04454567c5ff7f7
Ausaf-10 <102360335+Ausaf-10@users.noreply.github.com>
2023-07-06 16:53:15 +0530

    Time: 16 ms (80.93%), Space: 13.4 MB (96.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,16 +4,15 @@ class Solution {
 public:
 
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // priority_queue < pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > minh;
 
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minHeap;
-        unordered_map <int,int> mp;
-        vector <int> ans;
+        unordered_map <int,int> mp; // TO STORE THE COUNT OF EVERY ELEMENT IN ARRAY
+        vector <int> ans; // TO STORE THE ANS 
         
         for(int i=0; i<nums.size(); i++)  mp[nums[i]]++;
            
         for(auto number:mp){
-            minHeap.push({number.second, number.first});
+            minHeap.push({number.second, number.first}); // PUSHING THE PAIR BY COUNT FIRST THEN THE NUMBER ALONG WITH IT 
             if(minHeap.size() > k)
                 minHeap.pop();
         }
