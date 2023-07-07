cdc38e80366fabe5e5efbea01033c51afa60746e
Ausaf-10 <102360335+Ausaf-10@users.noreply.github.com>
2023-07-07 11:28:59 +0530

    Time: 15 ms (20.83%), Space: 11.3 MB (52.93%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
+        // THIS QUESTION IS THE SAMES AS ***TOP K FREQUENT ELEMENTS*** BUT IN THIS WE ARE TAKING
+        // MAXHEAP BECAUSE WHEN THE COUNT OF 2 ELEMENTS ARE SAME THEN THE GREATER WILL BE 
+        // AT THE TOP SO WE WOULD NOT HAVE TO DO ANYHTING EXTRA!!!
         priority_queue<pair<int,int>> maxHeap;
         vector<int> ans;
         unordered_map<int,int> mp;
 
         for (int i=0; i<nums.size(); i++) mp[nums[i]]++;
 
+        // NOW PUTTING ELEMENTS FROM MAP TO MAXHEAP ACCORDING TO THEIR FREQUENCY
         for (auto number:mp){
             maxHeap.push({-1*number.second,number.first});
         }
 
+
+        // NOW PUTTING ALL THE ELEMENTS FROM MAXHEAP TO ANS 
         while (!maxHeap.empty()){
             int freq = abs(maxHeap.top().first);
             int currentValue = maxHeap.top().second;
