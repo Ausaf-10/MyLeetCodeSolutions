8cfe7df9a2c5b1442fa4c5d6a084838df587b168
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-23 19:29:37 +0530

    Time: 32 ms (39.41%), Space: 15.5 MB (26.22%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,55 @@
+
+class DisjointSet{
+    public:
+        vector<int> parent,size;
+        DisjointSet(int n){
+            size.resize(n+1,1);
+           
+            parent.resize(n+1,0);
+            for (int i=0; i<=n; i++) parent[i] = i;
+        }
+
+        int findUltimateParent(int node){
+            if (parent[node] == node) return node;
+            return parent[node] = findUltimateParent(parent[node]);
+        }
+
+        void UnionBySize(int u, int v){
+            int ulp_u = findUltimateParent(u);
+            int ulp_v = findUltimateParent(v);
+            if (ulp_u == ulp_v) return ;
+            if (size[ulp_u] < size[ulp_v]){
+                size[ulp_v]+=size[ulp_u];
+                parent[ulp_u] = parent[ulp_v]; 
+            }
+            else{
+                size[ulp_u]+=size[ulp_v];
+                parent[ulp_v] = parent[ulp_u]; 
+            }
+        }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& adj) {
+        int V = adj.size();
+        DisjointSet ds(V);
+        for (int i=0; i<V; i++){
+            for (int j=0; j<V; j++){
+                if (adj[i][j] == 1){
+                    ds.UnionBySize(i,j);
+                }
+            }
+        }
+        int cnt = 0 ;
+        // for (int i=0; i<V; i++){
+        //     ds.parent[i] = ds.findUltimateParent(i);
+        // }
+        for (int i=0; i<V; i++){
+            if (ds.findUltimateParent(i) == i) cnt++;
+        }
+        
+        return cnt;
+        
+    }
+};
