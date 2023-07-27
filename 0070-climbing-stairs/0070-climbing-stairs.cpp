5b391ae0733e988afa21943542951a1900d24e19
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-27 12:44:57 +0530

    Time: 3 ms (35.37%), Space: 5.9 MB (72.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    int fun(int n, vector<int>& dp){
-        if (n<=0) return 0;
-        if (n == 1 || n==2 ) return n;
-        if (dp[n]!=-1) return dp[n];
-        return dp[n] = fun(n-1,dp) + fun(n-2,dp);
-    }
+    // int fun(int n, vector<int>& dp){
+    //     if (n<=0) return 0;
+    //     if (n == 1 || n==2 ) return n;
+    //     if (dp[n]!=-1) return dp[n];
+    //     return dp[n] = fun(n-1,dp) + fun(n-2,dp);
+    // }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return fun(n,dp);
+        // vector<int> dp(n+1,-1);
+        // dp[0] = 0, dp[1] = 1, dp[2] = 2;
+        // if (n == 0 || n==1 || n==2 ) return dp[n];
+        // for (int i=3; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        int a  = 1, b = 2;
+        
+        if (n == 1 || n == 2) return n;
+
+        for (int i=3; i<=n ; i++){
+            int c = a + b;
+            a = b , b = c;
+        }
+
+        return b;
     }
 };
