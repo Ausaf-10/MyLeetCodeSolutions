2801cc90d54d2d31bbbf7966a2b6de25c591e76f
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 11:58:19 +0530

    Time: 5 ms (24.83%), Space: 7.8 MB (24.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -29,10 +29,42 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        if (obstacleGrid[0][0] == 1) return 0;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
 
-        vector<vector<int>> memoizationDP(n,vector<int>(m,-1));
-        return memoization(n-1,m-1, n, m, obstacleGrid,memoizationDP);
+        // vector<vector<int>> memoizationDP(n,vector<int>(m,-1));
+        // vector<vector<int>> tabulationDP(n,vector<int>(m,0));
+
+        // for (int i=0; i<n; i++){
+        //     for (int j=0; j<m; j++){
+        //         if (i == 0 && j == 0) tabulationDP[i][j] = 1;
+        //         else{
+        //             int left = 0, up = 0;
+        //             if (i > 0 && obstacleGrid[i-1][j]!=1) up = tabulationDP[i-1][j];
+        //             if (j > 0 && obstacleGrid[i][j-1]!=1) left = tabulationDP[i][j-1];
+        //             tabulationDP[i][j] = left + up;
+        //         }
+        //     }
+        // }
+
+        // return tabulationDP[n-1][m-1];
+        
+        // SPACE OPTIMIZATION
+        vector<int> prev(m,0);
+        for (int row = 0; row< n; row++){
+            vector<int> temp(m,0);
+            for (int col = 0; col< m; col++){
+                if (row == 0 && col == 0) temp[col] = 1;
+                else{
+                    int left = 0, up = 0;
+                    if (row > 0 && obstacleGrid[row-1][col]!=1) up = prev[col];
+                    if (col > 0 && obstacleGrid[row][col-1]!=1) left = temp[col-1];
+                    temp[col] = left + up;
+                }
+            }
+            prev = temp;
+        }
+        return prev[m-1];
+        // return memoization(n-1,m-1, n, m, obstacleGrid,memoizationDP);
         // return recursion(n-1,m-1, n, m, obstacleGrid);
     }
 };
