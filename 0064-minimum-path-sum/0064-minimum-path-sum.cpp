ba5decebd1fc3767136e1ac586d7de75a84ed1c4
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 13:20:50 +0530

    Time: 3 ms (99.58%), Space: 9.9 MB (71.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -25,18 +25,34 @@ public:
         // vector<vector<int>> dp(n, vector<int> (m , -1));
         // return memoization(n-1, m-1, grid, dp);
 
-        vector<vector<int>> tabulationDP(n, vector<int> (m , -1));
-        for (int row=0; row<n; row++){
+        // vector<vector<int>> tabulationDP(n, vector<int> (m , -1));
+        // for (int row=0; row<n; row++){
+        //     for (int col=0; col<m; col++){
+        //         if (row == 0 && col == 0) tabulationDP[0][0] = grid[0][0];
+        //         else{
+        //             int left = INT_MAX, up = INT_MAX;
+        //             if (row > 0) up = tabulationDP[row-1][col];
+        //             if (col > 0) left = tabulationDP[row][col-1];
+        //             tabulationDP[row][col] = grid[row][col] + min(up,left);
+        //         }
+        //     }
+        // }
+        // return tabulationDP[n-1][m-1];
+
+        vector<int> prev(m,0);
+        for (int row=0; row<n ; row++){
+            vector<int> temp(m,0);
             for (int col=0; col<m; col++){
-                if (row == 0 && col == 0) tabulationDP[0][0] = grid[0][0];
+                if (row == 0 && col == 0) temp[col] = grid[0][0];
                 else{
                     int left = INT_MAX, up = INT_MAX;
-                    if (row > 0) up = tabulationDP[row-1][col];
-                    if (col > 0) left = tabulationDP[row][col-1];
-                    tabulationDP[row][col] = grid[row][col] + min(up,left);
+                    if (row > 0) up = prev[col];
+                    if (col > 0) left = temp[col-1];
+                    temp[col] = grid[row][col] + min(up,left);
                 }
             }
+            prev = temp;
         }
-        return tabulationDP[n-1][m-1];
+        return prev[m-1];
     }
 };
