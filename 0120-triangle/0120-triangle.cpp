a80be7f38289acfa2080c16fb762fa5238c89a87
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 15:39:29 +0530

    Time: 4 ms (87.73%), Space: 9.4 MB (6.45%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -18,8 +18,41 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
 
-        vector<vector<int>> memoizationDP (n , vector<int>(n,-1));
+        // vector<vector<int>> memoizationDP (n , vector<int>(n,-1));
+        vector<vector<int>> tabulationDP (n , vector<int>(n,0));
+        // TABULATION
+        
+        // 1. WRITE THE BASE CASE
+        // for (int j=0; j<n; j++){
+        //     tabulationDP[n-1][j] = triangle[n-1][j];
+        // }
+        // // 2. START AFTER THE BASE CASE
+        // for (int row = n-2; row >= 0; row--){
+        //     for (int col = row ; col >= 0; col--){
+                
+        //         int down = tabulationDP[row+1][col];
+        //         int diagonal = tabulationDP[row+1][col+1];
+        //         tabulationDP[row][col] = triangle[row][col] + min(down,diagonal);
+                
+        //     }
+        // }
+        // return tabulationDP[0][0];
+        vector<int> prev(n,0);
+        for (int j=0; j<n; j++){
+            prev[j] = triangle[n-1][j];
+        }
 
-        return memoization(0,0,triangle,n, memoizationDP);
+        for (int row = n-2; row >= 0; row--){
+            vector<int> temp(n,0);
+            for (int col = row ; col >= 0; col--){
+                int down = prev[col];
+                int diagonal = prev[col+1];
+                temp[col] = triangle[row][col] + min(down,diagonal);
+            }
+            prev = temp;
+        }
+
+        return prev[0];
+        // return memoization(0,0,triangle,n, memoizationDP);
     }
 };
