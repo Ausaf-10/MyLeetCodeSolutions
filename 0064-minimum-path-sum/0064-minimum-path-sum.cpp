e07f3908d114ae62dc43395c549d0eea4f3b9165
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 13:13:10 +0530

    Time: 11 ms (64.92%), Space: 10.1 MB (43.54%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int recursion(int row, int col, vector<vector<int>>& grid){
+        if (row == 0 && col == 0) return grid[0][0];
+        int left = INT_MAX, up = INT_MAX;
+        if (row > 0) up = recursion(row-1,col,grid);
+        if (col > 0) left = recursion(row,col-1,grid);
+
+        return grid[row][col] + min(up,left);
+    }
+
+    int memoization(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
+        if (row == 0 && col == 0) return grid[0][0];
+        if (dp[row][col]!=-1) return dp[row][col];
+        int left = INT_MAX, up = INT_MAX;
+        if (row > 0) up = memoization(row-1,col,grid,dp);
+        if (col > 0) left = memoization(row,col-1,grid,dp);
+
+        return dp[row][col] = grid[row][col] + min(up,left);
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int> (m , -1));
+        return memoization(n-1, m-1, grid, dp);
+    }
+};
