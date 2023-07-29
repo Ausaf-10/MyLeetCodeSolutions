c3fc48de38e143b3c3a28ff55b7ac7a6716b70fc
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 13:17:49 +0530

    Time: 7 ms (93.26%), Space: 10.1 MB (43.54%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -22,7 +22,21 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int> (m , -1));
-        return memoization(n-1, m-1, grid, dp);
+        // vector<vector<int>> dp(n, vector<int> (m , -1));
+        // return memoization(n-1, m-1, grid, dp);
+
+        vector<vector<int>> tabulationDP(n, vector<int> (m , -1));
+        for (int row=0; row<n; row++){
+            for (int col=0; col<m; col++){
+                if (row == 0 && col == 0) tabulationDP[0][0] = grid[0][0];
+                else{
+                    int left = INT_MAX, up = INT_MAX;
+                    if (row > 0) up = tabulationDP[row-1][col];
+                    if (col > 0) left = tabulationDP[row][col-1];
+                    tabulationDP[row][col] = grid[row][col] + min(up,left);
+                }
+            }
+        }
+        return tabulationDP[n-1][m-1];
     }
 };
