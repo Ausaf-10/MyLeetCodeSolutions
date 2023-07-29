93bf4b76c00e79977c90488e16a3c3482be6cced
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 12:20:21 +0530

    Time: 2 ms (35.97%), Space: 5.9 MB (72.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,25 +17,25 @@ public:
         return dp[n] = first + two;
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        // dp[0] = 0, dp[1] = 1, dp[2] = 2;
-        // if (n == 0 || n==1 || n==2 ) return dp[n];
-        // for (int i=3; i<=n; i++){
-        //     dp[i] = dp[i-1] + dp[i-2];
+        // vector<int> dp(n+1,-1);
+        // dp[0] = 1;
+        // for (int i=1; i<=n; i++){
+        //     int first = dp[i-1];
+        //     int two = 0;
+        //     if (i > 1) two = dp[i-2];
+        //     dp[i] = first + two;
         // }
         // return dp[n];
 
-        return memoization(n,dp);
+        // return memoization(n,dp);
 
-        // int a  = 1, b = 2;
+        int prev  = 1, prev2 = 0;
         
-        // if (n == 1 || n == 2) return n;
+        for (int i=1; i<=n ; i++){
+            int cur = prev + prev2;
+            prev2 = prev , prev = cur;
+        }
 
-        // for (int i=3; i<=n ; i++){
-        //     int c = a + b;
-        //     a = b , b = c;
-        // }
-
-        // return b;
+        return prev;
     }
 };
