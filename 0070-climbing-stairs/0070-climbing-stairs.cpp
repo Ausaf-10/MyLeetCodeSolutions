419818af9fe2f4410cb14752090eb9231e64c3f0
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-29 12:13:29 +0530

    Time: 2 ms (35.97%), Space: 6.2 MB (36.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    // int fun(int n, vector<int>& dp){
-    //     if (n<=0) return 0;
-    //     if (n == 1 || n==2 ) return n;
-    //     if (dp[n]!=-1) return dp[n];
-    //     return dp[n] = fun(n-1,dp) + fun(n-2,dp);
-    // }
+    int recursion(int n){
+        if (n == 0) return 1;
+        int first = recursion(n-1);
+        int two = 0;
+        if (n > 1) two = recursion(n-2);
+        return first + two;
+    }
+
+    int memoization(int n, vector<int>& dp){
+        if (n == 0) return 1;
+        if (dp[n]!=-1) return dp[n];
+        int first = memoization(n-1,dp);
+        int two = 0;
+        if (n > 1) two = memoization(n-2,dp);
+        return dp[n] = first + two;
+    }
     int climbStairs(int n) {
-        // vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,-1);
         // dp[0] = 0, dp[1] = 1, dp[2] = 2;
         // if (n == 0 || n==1 || n==2 ) return dp[n];
         // for (int i=3; i<=n; i++){
@@ -15,15 +25,17 @@ public:
         // }
         // return dp[n];
 
-        int a  = 1, b = 2;
+        return memoization(n,dp);
+
+        // int a  = 1, b = 2;
         
-        if (n == 1 || n == 2) return n;
+        // if (n == 1 || n == 2) return n;
 
-        for (int i=3; i<=n ; i++){
-            int c = a + b;
-            a = b , b = c;
-        }
+        // for (int i=3; i<=n ; i++){
+        //     int c = a + b;
+        //     a = b , b = c;
+        // }
 
-        return b;
+        // return b;
     }
 };
