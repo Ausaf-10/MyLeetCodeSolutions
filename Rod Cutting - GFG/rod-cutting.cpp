f272bd6fa773284f289c345d309116c540277ba7
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-07-31 23:16:45 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -55,10 +55,29 @@ class Solution{
         return dp[n-1][n];
     }
     
+    int spaceOptimisation(int price[], int n){
+        vector<int> prev(n+1,0);
+        for (int i=1 ; i<=n; i++) prev[i] = i * price[0];
+        
+        for (int indx = 1; indx < n; indx++){
+            for (int target = 1; target <= n; target++){
+                int notPick = 0 + prev[target];
+                int pick = 0; 
+                int rodLength = indx + 1;
+                
+                if (rodLength <= target) pick = price[indx] + prev[target - rodLength];
+                
+                prev[target] = max(pick,notPick);
+            }
+        }
+        
+        return prev[n];
+    }
+    
     int cutRod(int price[], int n) {
         //code her0      
         vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return tabulation(price, n);
+        return spaceOptimisation(price, n);
     }
 };
 
