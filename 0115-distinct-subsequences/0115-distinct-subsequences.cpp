ff4efd4ca9e198dc085728900a23ce1fbd2e9c66
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-01 23:53:10 +0530

    Time: 38 ms (80.3%), Space: 30.1 MB (37.88%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -20,9 +20,29 @@ public:
         return dp[indx1][indx2] = memoization(indx1-1, indx2, str1, str2, dp);
     }
 
+    int tabulation(string str1, string str2){
+        int n = str1.size(), m = str2.size();
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1,0));
+        // BASE CASE
+        for (int j=1; j<=m; j++) dp[0][j] = 0;
+        for (int i=0; i<=n; i++) dp[i][0] = 1;
+
+        for (int indx1 = 1; indx1<=n; indx1++){
+            for (int indx2 = 1; indx2<=m; indx2++){
+                if (str1[indx1-1] == str2[indx2-1]){
+                    dp[indx1][indx2] = (dp[indx1-1][indx2] + dp[indx1-1][indx2-1])%(int)(1e9+7);
+                    // dp[indx1][indx2]+=dp[indx1-1][indx2-1];
+                }
+            
+                else dp[indx1][indx2] =dp[indx1-1][indx2];
+            }
+            
+        }
+
+        return dp[n][m];
+    }
+
     int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return memoization(n, m, s, t, dp);
+        return tabulation(s, t);
     }
 };
