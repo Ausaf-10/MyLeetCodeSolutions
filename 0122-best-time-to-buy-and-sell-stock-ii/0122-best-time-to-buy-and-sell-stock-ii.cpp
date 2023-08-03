19a75fbcfc45106f1052d39cca0bd3b8027d54ab
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-03 17:43:07 +0530

    Time: 17 ms (15.98%), Space: 17.2 MB (17.21%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -25,9 +25,29 @@ public:
         return dp[indx][buy] = profit;
     }
 
+    int tabulation(vector<int>& prices){
+        int n = prices.size();
+        vector<vector<int>> dp (n+1, vector<int>(2,-1));
+        dp[n][0] = 0, dp[n][1] = 0;
+        for (int i = n-1; i>=0; i--){
+            for (int j=0; j<2; j++){
+                int profit = 0;
+                if (j){
+                    profit = max(-prices[i] + dp[i+1][!j], 0 + dp[i+1][j]);
+                }
+                else{
+                    profit = max(prices[i] + dp[i+1][!j], 0 + dp[i+1][j]);
+                }
+                dp[i][j] = profit;
+            }
+        } 
+
+        return dp[0][1];
+    }
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>> dp (n+1, vector<int>(2,-1));
-        return memoization(0, 1, prices, dp);
+        return tabulation(prices);
     }
 };
