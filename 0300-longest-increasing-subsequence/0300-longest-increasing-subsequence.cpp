b94cd95dbbdc03e8756bb6ef65e8f55b83fdda8a
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-04 14:36:17 +0530

    Time: 518 ms (41.02%), Space: 572.1 MB (7.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int recursion(int indx, int prevIndx, vector<int>& arr){
         if (indx == arr.size()) return 0;
+        
         int len = 0 + recursion(indx+1, prevIndx, arr);
+        
         if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
             len = max(len, 1 + recursion(indx+1, indx, arr));
         } 
@@ -12,9 +14,11 @@ public:
 
     int memoization(int indx, int prevIndx, vector<int>& arr, vector<vector<int>>& dp){
         if (indx == arr.size()) return 0;
+        
         if (dp[indx][prevIndx+1]!=-1) return dp[indx][prevIndx+1];
 
         int len = 0 + memoization(indx+1, prevIndx, arr, dp);
+        
         if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
             len = max(len, 1 + memoization(indx+1, indx, arr, dp));
         } 
@@ -22,10 +26,29 @@ public:
         return dp[indx][prevIndx + 1] = len;
     }
 
+    int tabulation(vector<int>& arr){
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+        for (int indx = n-1; indx>=0; indx--){
+            for (int prevIndx = indx - 1; prevIndx>=-1; prevIndx--){
+                int len = 0 + dp[indx+1][prevIndx + 1];
+                
+                if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
+                    len = max(len, 1 + dp[indx+1][indx + 1]);
+                } 
+
+                dp[indx][prevIndx + 1] = len;
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
-        return memoization(0, -1, nums, dp);    
-        // return recursion(0, -1, nums);
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        // return memoization(0, -1, nums, dp);    
+        return tabulation(nums);
     }
 };
