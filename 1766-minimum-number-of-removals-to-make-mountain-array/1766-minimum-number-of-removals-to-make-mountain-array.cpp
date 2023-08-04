fa6cbc9359118351381ea7580dcff92d4a105f47
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-04 22:16:28 +0530

    Time: 15 ms (99.31%), Space: 12.7 MB (7.52%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
-    vector<int> longestIncreasingSubsequence(vector<int>& nums){
-       int n = nums.size();
-       vector<int> dp(n,1);
-       for (int i=0; i<n; i++){
-           for (int j=0; j<i; j++){
-               if (nums[i] > nums[j] && dp[i] < dp[j] + 1){
-                   dp[i] = dp[j] + 1;
-               }
-           }
-       }
+    vector<int> longestIncreasingSubsequence(vector<int>& arr){
+        int n = arr.size();
+        
+        vector<int> dp(n,1);
+        vector<int> temp;
+        
+        temp.push_back(arr[0]);
+        
+        for (int i=1; i<n; i++){
+            if (arr[i] > temp.back()){
+                temp.push_back(arr[i]);
+            }
+            else{
+                int indx = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[indx] = arr[i];
+            }
+            dp[i] = temp.size();
+        }
        
        return dp;
 	}
 	 
-	vector<int> longestDecreasingSubsequence(vector<int>& nums){	       
-	       int n = nums.size();
-	       vector<int> dp(n,1);
-	    for (int i=n-1; i>=0; i--){
-            for (int j=n-1; j>i; j--){
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1){
-                   dp[i] = dp[j] + 1;
-                }
-           }
-       }
-	       
-	   return dp;
+	vector<int> longestDecreasingSubsequence(vector<int>& arr){	       
+	    int n = arr.size();
+        
+        vector<int> dp(n,1);
+        vector<int> temp;
+        
+        temp.push_back(arr[n-1]);
+        
+        for (int i=n-2; i>=0; i--){
+            if (arr[i] > temp.back()){
+                temp.push_back(arr[i]);
+            }
+            else{
+                int indx = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[indx] = arr[i];
+            }
+            dp[i] = temp.size();
+        }
+       
+       return dp;
 	}
 
     int minimumMountainRemovals(vector<int>& nums) {
@@ -35,7 +51,7 @@ public:
 	    vector<int> LDS = longestDecreasingSubsequence(nums);
 
         vector<int> temp(n,0);
-	    int maxi = 1;
+	    int maxi = -1e9;
 	    for (int i=0; i<n; i++){
 	        if (LIS[i] > 1 && LDS[i] > 1){
                 temp[i] = LIS[i] + LDS[i] - 1;
@@ -46,6 +62,6 @@ public:
 
         return n - maxi;
         
-        // return cnt1 + cnt2;
+        
     }
 };
