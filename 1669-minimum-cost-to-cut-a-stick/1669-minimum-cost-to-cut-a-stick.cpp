0d09460109e8c33ad83a753d8504c1054987c8c1
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-05 20:54:46 +0530

    Time: 42 ms (96.5%), Space: 12.1 MB (6.97%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -26,6 +26,24 @@ public:
     
     }
 
+    int tabulation(int n , vector<int>& cuts, int c){
+        vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+        
+        for(int i=c; i>=1; i--){
+            for(int j=1; j<=c; j++){ 
+                if(i>j) continue;
+                int mini = INT_MAX;
+                for(int ind=i; ind<=j; ind++){
+                    int ans = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(mini, ans);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        
+        return dp[1][c];
+    }
+
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size();
         vector<vector<int>> dp(c+1, vector<int>(c+1,-1));
@@ -33,6 +51,6 @@ public:
         cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
         
-        return memoization(1, c, cuts, dp);
+        return tabulation(n, cuts, c);
     }
 };
