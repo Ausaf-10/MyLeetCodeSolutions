b2e6e2bf57e838250ea22c8b6d369e7d474b492e
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-05 20:37:14 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -40,6 +40,7 @@ public:
         
         for (int i=N-1; i>=1; i--){
             for (int j=i+1; j<N; j++){
+                if (i > j) continue;
                 int mini = 1e9;
                 for (int k = i; k < j; k++){
                     int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
