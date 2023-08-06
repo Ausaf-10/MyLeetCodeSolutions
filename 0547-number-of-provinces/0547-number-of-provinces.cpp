88608e730d198c53303f5d23f90e33fe43665702
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-06 14:21:07 +0530

    Time: 28 ms (75.43%), Space: 16.6 MB (33.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,55 +1,46 @@
-
-class DisjointSet{
-    public:
-        vector<int> parent,size;
-        DisjointSet(int n){
-            size.resize(n+1,1);
-           
-            parent.resize(n+1,0);
-            for (int i=0; i<=n; i++) parent[i] = i;
-        }
-
-        int findUltimateParent(int node){
-            if (parent[node] == node) return node;
-            return parent[node] = findUltimateParent(parent[node]);
-        }
-
-        void UnionBySize(int u, int v){
-            int ulp_u = findUltimateParent(u);
-            int ulp_v = findUltimateParent(v);
-            if (ulp_u == ulp_v) return ;
-            if (size[ulp_u] < size[ulp_v]){
-                size[ulp_v]+=size[ulp_u];
-                parent[ulp_u] = parent[ulp_v]; 
-            }
-            else{
-                size[ulp_u]+=size[ulp_v];
-                parent[ulp_v] = parent[ulp_u]; 
-            }
-        }
-};
-
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& adj) {
-        int V = adj.size();
-        DisjointSet ds(V);
-        for (int i=0; i<V; i++){
-            for (int j=0; j<V; j++){
-                if (adj[i][j] == 1){
-                    ds.UnionBySize(i,j);
+    void bfs(int node, vector<int>& vis, vector<int> adj[]){
+        queue<int> q;
+        q.push(node);
+        vis[node]=1;
+
+        while (!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for (auto it : adj[node]){
+                if (!vis[it]){
+                    q.push(it);
+                    vis[it] = 1;
                 }
             }
         }
-        int cnt = 0 ;
-        for (int i=0; i<V; i++){
-            int x = ds.findUltimateParent(i);
-        }
-        for (int i=0; i<V; i++){
-            if (ds.parent[i] == i) cnt++;
-        }
-        
-        return cnt;
-        
+
+        return ;
     }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+       
+        vector<int> adj[n];
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if (isConnected[i][j] == 1 && i!=j){
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        int cnt = 0;
+        vector<int> vis(n,0);
+        for (int i=0; i<n; i++){
+            if (!vis[i]){
+                bfs(i,vis,adj);
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
 };
