912fc88e504157c9dfd6dc462735c11667e9eb88
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-07 12:01:14 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,57 +8,39 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    void dfs(vector<pair<int,int>> adj[], int node, vector<int>& vis, stack<int>& stk){
-        vis[node] = 1;
-        for (auto it : adj[node]){
-            int neighbor = it.first;
-            int wt = it.second;
-            if (!vis[neighbor]){
-                dfs(adj,neighbor,vis,stk);
-            }
-        }
-        
-        stk.push(node);
-        return ;
-    }
-     vector<int> shortestPath(int V,int M, vector<vector<int>>& edges){
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
         // code here
-        vector<pair<int,int>> adj[V];
-        for (int i=0; i<M; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-            
+        vector<pair<int,int>> adj[N];
+        
+        for (auto it : edges){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
             adj[u].push_back({v,wt});
         }
-        stack<int> stk;
-        vector<int> vis(V,0);
-        for (int i=0; i<V; i++){
-            if (!vis[i]){
-                dfs(adj,i,vis,stk);
-            }
-        }
         
-        vector<int> dist(V,0);
-        for (int i=0; i < V; i++) dist[i] = 1e9;
-        dist[0] = 0;
-        
-        while (!stk.empty()){
-            int node = stk.top();
-            stk.pop();
+        vector<int> dis(N,1e9);
+        queue<int> q; q.push(0); dis[0] = 0;
+        while (!q.empty()){
+            int node = q.front();
+            q.pop();
             
             for (auto it : adj[node]){
                 int v = it.first;
                 int wt = it.second;
-                dist[v] = min(dist[v], wt + dist[node]);
+                if (dis[v] > dis[node] + wt){
+                    dis[v] = dis[node] + wt;
+                    q.push(v);
+                }
             }
+            
         }
         
-        for (int i=0; i<V; i++){
-            if (dist[i] == 1e9) dist[i] = -1;
+        for (int i=0; i<N; i++){
+            if (dis[i] == 1e9) dis[i] = -1;
         }
         
-        return dist;
+        return dis;
     }
 };
 
