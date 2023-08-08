fab4f4fb59b5be866d4f6b069147e4fc565f1d50
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-08 12:28:15 +0530

    Time: 0 ms (100%), Space: 6.6 MB (19.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,62 +1,37 @@
 class Solution {
 public:
-    // TC: O(2^N*M), SC:O(N-1 + M-1)
-    int recursion(int row, int col, int n, int m){
+    int recursion(int row, int col){
         if (row == 0 && col == 0) return 1;
-        if (row < 0 || col < 0 ) return 0;
-        int upwards = recursion(row-1,col,n,m);
-        int left =  recursion(row,col-1,n,m);
-        return upwards + left;
+        int up = 0, left = 0;
+        if (row > 0) up = recursion(row-1, col);
+        if (col > 0) left = recursion(row, col-1);
+        return up + left;
     }
-
-    // MEMOIZATION -> TC:O(N*M), SC:O(N*M) + O(N-1 + M-1)
-    int memoization(int row, int col, int n, int m, vector<vector<int>> &dp){
+    int memoization(int row, int col, vector<vector<int>>& dp){
         if (row == 0 && col == 0) return 1;
-        if (row < 0 || col < 0 ) return 0;
         if (dp[row][col]!=-1) return dp[row][col];
-        
-        int upwards = memoization(row-1,col,n,m,dp);
-        int left =  memoization(row,col-1,n,m,dp);
-        
-        return dp[row][col] = upwards + left;
+        int up = 0, left = 0;
+        if (row > 0) up = memoization(row-1, col, dp);
+        if (col > 0) left = memoization(row, col-1, dp);
+        return dp[row][col] = up + left;
     }
-
-    int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m, vector<int> (n,-1));
-
-
-        // TABULATION -> TC:O(N*M), SC:O(N*M)
-        // vector<vector<int>> dp(m, vector<int> (n,0));
-        // for (int row=0; row < m; row++){
-        //     for (int col = 0; col<n; col++){
-        //         if (row == 0 && col == 0) dp[0][0] = 1;
-        //         else{
-        //             int left = 0, upwards = 0;
-        //             if (row > 0) upwards = dp[row-1][col];
-        //             if (col > 0)  left = dp[row][col-1];
-        //             dp[row][col] = left + upwards;
-        //         }
-        //     }
-        // }
-
-        // return dp[m-1][n-1];
-
-        // SPACE OPTIMIZATION
-        vector<int> prev(n,0);
-        for (int row=0; row < m; row++){
-            vector<int> temp(n,0);
-            for (int col = 0; col<n; col++){
-                if (row == 0 && col == 0) temp[0] = 1;
+    int tabulation(int n , int m){
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        for (int row=0;row<n; row++){
+            for (int col=0; col<m; col++){
+                if (row == 0 && col == 0) dp[row][col] = 1;
                 else{
-                    int left = 0, upwards = 0;
-                    if (row > 0) upwards = prev[col];
-                    if (col > 0)  left = temp[col-1];
-                    temp[col] = left + upwards;
+                    int up = 0, left = 0;
+                    if (row > 0) up = dp[row-1][col];
+                    if (col > 0) left = dp[row][col-1];
+                    dp[row][col] = up + left;
                 }
             }
-            prev = temp;
         }
 
-        return prev[n-1];
+        return dp[n-1][m-1];
+    }
+    int uniquePaths(int m, int n) {
+        return tabulation(m,n);
     }
 };
