f3bef51e56f3d86d15b1faf15c98188d8d4482fd
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-11 12:17:58 +0530

    Time: 34 ms (48.39%), Space: 29.7 MB (5.92%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -17,16 +17,37 @@ public:
             if (target % coins[0] == 0) return 1;
             return 0;
         }
+        
         if (dp[indx][target]!=-1) return dp[indx][target];
+        
         int notPick = memoization(indx-1, target, coins, dp);
         int pick = 0;
         if (coins[indx] <= target) pick = memoization(indx, target - coins[indx], coins, dp);
         return dp[indx][target] = pick + notPick;
     }
 
+    int tabulation(int amount, vector<int>& coins){
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1,0));
+        for (int i=0; i<n; i++) dp[i][0] = 1;
+        for (int j=0; j<=amount; j++){
+            dp[0][j] = j % coins[0] == 0;
+        }
+
+        for (int indx=1; indx<n; indx++){
+            for (int target=1; target<=amount; target++){
+                int notPick = dp[indx-1][target];
+                int pick = 0;
+                if (coins[indx] <= target) pick = dp[indx][target - coins[indx]] ;
+                dp[indx][target] = pick + notPick;
+            }
+        }
+        return dp[n-1][amount];
+    }
+
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return memoization(n-1, amount, coins, dp);
+        return tabulation(amount, coins);
     }
 };
