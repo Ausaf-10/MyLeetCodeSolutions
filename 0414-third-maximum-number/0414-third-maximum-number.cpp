b79a24d838fdf3a5dc6ccd0326349f424689f498
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-11 19:12:12 +0530

    Time: 7 ms (64.3%), Space: 9.1 MB (53.49%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int> st;
-        int maxi = -1e9;
-        for (auto it : nums){
-            maxi = max(maxi, it);
-            st.insert(it);
-        }
+        // WE WILL KEEP 3 MAXIMUMS AND WILL COMPARE EACH OF THEM WHENEVER WE NEED TO UPDATE THE VALUE AND WITH THE HELP OF VARIABLES
+        // ONLY WE WILL BE ABLE TO DO THIS QUESTION IN CONSTANT SPACE AND LINEAR TIME 
 
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (auto it : st){
-            pq.push(it);
-            if (pq.size() > 3) pq.pop();
-        }
-        if (pq.size() == 3) return pq.top();
-        
-        return maxi;
+        if (nums.size() == 1) return nums[0]; // SIZE 1
+        else if (nums.size() == 2) return max(nums[0], nums[1]);
+
+        long m1 = LONG_MIN, m2 = LONG_MIN, m3 = LONG_MIN;
+        int n = nums.size();
+        for (auto it : nums){
+            if (it == m1 || it == m2 || it == m3) continue; // FOR UNIQUENESS
+            if (it > m1){
+                m3 = m2, m2 = m1, m1 = it;
+            }
+            else if (it > m2){
+                m3 = m2, m2 = it;
+            }
+            else if (it > m3){
+                m3 = it;
+            }
+        } 
+
+        if (m3 == LONG_MIN ) return (int)m1;
+        return (int) m3; 
     }
 };
