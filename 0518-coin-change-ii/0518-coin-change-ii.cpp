8310cfc7c55bd34750758e2708e374d87878cd43
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-11 12:12:50 +0530

    Time: 8 ms (92%), Space: 18.5 MB (50.23%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int recursion(int indx, int target, vector<int>& coins){
+        if (target == 0) return 1;
+        if (indx == 0){
+            return (target % coins[0] == 0);
+        }
+        int notPick = recursion(indx-1, target, coins);
+        int pick = 0;
+        if (coins[indx] <= target) pick = recursion(indx, target - coins[indx], coins);
+        return pick + notPick;
+    }
+
+    int memoization(int indx, int target, vector<int>& coins, vector<vector<int>>& dp){
+        if (target == 0) return 1;
+        if (indx == 0){
+            if (target % coins[0] == 0) return 1;
+            return 0;
+        }
+        if (dp[indx][target]!=-1) return dp[indx][target];
+        int notPick = memoization(indx-1, target, coins, dp);
+        int pick = 0;
+        if (coins[indx] <= target) pick = memoization(indx, target - coins[indx], coins, dp);
+        return dp[indx][target] = pick + notPick;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+        return memoization(n-1, amount, coins, dp);
+    }
+};
