5c0a28b75de59fb408d793181e085a695ca32cd3
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-13 15:46:22 +0530

    Time: 7 ms (23.86%), Space: 11.1 MB (57.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int search(vector<int>& A, int target) {
-        int n = A.size();
-        int lo=0,hi=n-1;
-        
-        while(lo<hi){
-            int mid=(lo+hi)/2;
-            if(A[mid]>A[hi]) lo=mid+1;
-            else hi=mid;
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target) return mid;
+            // LEFT SORTED
+            if (nums[low] <= nums[mid]){
+                if (nums[low] <= target && target<= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            // RIGHT SORTED
+            else{
+                if (nums[mid] <= target && target<= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
         }
-       
-        int rot=lo;
-        lo=0;hi=n-1;
-        
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int realmid=(mid+rot)%n;
-            if(A[realmid]==target)return realmid;
-            if(A[realmid]<target)lo=mid+1;
-            else hi=mid-1;
-        }
-        return -1;  
+
+        return -1;
     }
 };
