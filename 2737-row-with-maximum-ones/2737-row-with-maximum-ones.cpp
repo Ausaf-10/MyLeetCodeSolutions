d9764bce4ea8da453f738a668f09d1eef210a87a
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-16 18:26:48 +0530

    Time: 114 ms (87.09%), Space: 60.4 MB (87.91%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        int maxCnt = 0, indx = n;
-        for (int i=0; i<n; i++){
-            int cnt = 0;
-            for (int j = 0; j<m; j++){
-                if (mat[i][j] == 1){
-                    cnt++;
-                }
+    int getLowerbound(vector<int>& arr, int x){
+        int low =0, high = arr.size();
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (arr[mid] >= x){
+                high = mid-1;
             }
+            else low = mid + 1;
+        }
+        return low;
+    }
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxCnt = 0, n = mat.size(), m = mat[0].size();
+        int indx = -1;
+        
+        for (int i=0; i<n; i++){
+            sort(mat[i].begin(), mat[i].end());
+        }
+       
+        for (int i=0; i<n; i++){
+            int lb = lower_bound(mat[i].begin(), mat[i].end(),1) - mat[i].begin();
+            // int lb = getLowerbound(mat[i],1);
+            int cnt = m - lb;
             if (cnt > maxCnt){
                 maxCnt = cnt;
                 indx = i;
             }
-        }
+        }   
+
         if (maxCnt == 0) return {0,0};
         
-        return {indx,maxCnt};
+        return {indx, maxCnt};
     }
 };
