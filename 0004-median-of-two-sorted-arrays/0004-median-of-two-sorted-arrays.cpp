8b53585d4c70eca1f584eeac55fc11e0d10e6aa1
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-16 15:50:38 +0530

    Time: 39 ms (54.87%), Space: 89.3 MB (55.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
-        int i=0, j=0;
         int n1 = arr1.size(), n2 = arr2.size();
-        vector<int> temp;
-        while (i<n1 && j<n2){
+        int n = n1 + n2;
+        int indx1 = n/2 - 1, indx2 = n/2;
+        int ele1 = -1 , ele2 = -1;
+        int cnt = 0;
+
+        int i=0, j=0;
+        while (i < n1 && j < n2){
             if (arr1[i] <= arr2[j]){
-                temp.push_back(arr1[i]);
-                i++;
+                if (cnt == indx1) ele1 = arr1[i];
+                if (cnt == indx2) ele2 = arr1[i];
+                i++,cnt++;
             }
             else{
-                temp.push_back(arr2[j]);
-                j++;
+                if (cnt == indx1) ele1 = arr2[j];
+                if (cnt == indx2) ele2 = arr2[j];
+                j++,cnt++;
             }
-        }
-        while (i<n1){
-            temp.push_back(arr1[i]);
-            i++;
-        }
-        while (j<n2){
-            temp.push_back(arr2[j]);
-            j++;
-        }
+        }   
 
-        int size = temp.size();
-        if (size%2 != 0) return temp[size/2];
-        
-        double a = temp[size/2] , b = temp[size/2 - 1];
-        return (a+b)/2.0;
+        while (i < n1){
+            if (cnt == indx1) ele1 = arr1[i];
+            if (cnt == indx2) ele2 = arr1[i];
+            i++,cnt++;
+        }   
+
+        while (j < n2){
+            if (cnt == indx1) ele1 = arr2[j];
+            if (cnt == indx2) ele2 = arr2[j];
+            j++,cnt++;
+        } 
+
+        if (n%2 == 1) return ele2;
+
+        return (ele1+ele2)/(2.0);
     }
 };
