d3402344484c69ed8203f663e1a537ec523bdb31
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-16 21:59:51 +0530

    Time: 10 ms (99.96%), Space: 13.1 MB (70.5%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& mat, int k) {
-        int n = mat.size(), m = mat[0].size();
-        int low = mat[0][0], high = mat[n-1][m-1];
-        while (low <= high){
-            int mid = low + (high - low)/2;
-            int cnt = 0;
-            for (int i=0; i<n; i++){
-                cnt+=upper_bound(mat[i].begin(),mat[i].end(),mid)-mat[i].begin();
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = countLessEqual(matrix, mid);
+
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
-            if (cnt < k) low = mid + 1;
-            else high = mid - 1;
         }
+
         return low;
+    }
+
+private:
+    int countLessEqual(std::vector<std::vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int count = 0;
+        int row = n - 1;
+        int col = 0;
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= target) {
+                count += row + 1; // Count all elements in this column up to row
+                col++;
+            } else {
+                row--;
+            }
+        }
+
+        return count;
+    
     }
 };
