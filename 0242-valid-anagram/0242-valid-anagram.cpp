e2c6c5d7499dacf2655c264650bfc5846e54ce8f
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-17 13:12:24 +0530

    Time: 3 ms (98.25%), Space: 7.5 MB (5.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string pattern, string str) {
-        unordered_map<char,int> mp;
-        for (auto it : pattern) mp[it]++;
-
-        int cnt = mp.size();
-
-        for (auto ch : str){
-            if (mp.find(ch)!=mp.end()){
-                mp[ch]--;
-                if (mp[ch] == 0) cnt--;
-            }
-            else return false;
-        }
-
-        if (cnt == 0) return true;
+        int n = pattern.size(), m = str.size();
        
-        return false;
+        if (n!=m) return false;
+
+        int freq[26]={0};
+
+        for (int i=0; i<pattern.size(); i++){
+            freq[pattern[i]-'a']++;
+            freq[str[i]-'a']--;
+        }
+        
+        for (int i=0; i<26; i++){
+            if (freq[i] !=0) return false;
+        }
+        return true;
 
     }
 };
