9a9329c3b627fc64467547ddbeae743d505158cd
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-19 13:25:15 +0530

    Time: 11 ms (96.68%), Space: 20.4 MB (28.35%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -15,7 +15,6 @@ class Solution {
 public:
     void solve(TreeNode* root, unordered_map<ll,ll>& mp ,int target, ll prefixSum, ll& cnt){
         if (root == NULL) return ;
-        // mp[prefixSum]++;
         prefixSum+=root->val;
 
         ll rem = prefixSum - target;
@@ -23,16 +22,19 @@ public:
             cnt+=mp[rem];
         }
         
-        mp[prefixSum]++;
+        mp[prefixSum]++; // cnt badha do!
         solve(root->left, mp, target, prefixSum, cnt);
         solve(root->right, mp, target, prefixSum, cnt);
 
-        mp[prefixSum]--;
-        if (mp[prefixSum] == 0) mp.erase(prefixSum);
-        prefixSum-=root->val;
+        mp[prefixSum]--; // jab wapas ayenge toh iska ek count kam hojayega and do not remove straight away bass ek cnt kam hoga!! 
+        if (mp[prefixSum] == 0) mp.erase(prefixSum); // aur agar yeh poora zero hojayage tab aap remove kr skte ho !
+        prefixSum-=root->val; // iss node ki value ko decrement kr denge!
         return ;
     }
     int pathSum(TreeNode* root, int targetSum) {
+
+        // THIS QUESTION IS SIMILAR TO COUNT SUBARRAYS WITH SUM EQUAL K
+
         unordered_map<ll,ll> mp; // PREFIXX SUM -> OCCURENCE
         ll prefixSum = 0, cnt = 0;
         mp[0] = 1;
