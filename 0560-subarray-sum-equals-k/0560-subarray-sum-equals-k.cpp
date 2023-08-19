bda5fbbe7c32a2666bd2216a9429fe30c0f79ea4
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-19 13:10:49 +0530

    Time: 101 ms (50.93%), Space: 43.9 MB (46.84%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,8 +3,9 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         map<int,int> mp;
         int preSum=0,cnt=0;
+        mp[0] = 1;
         for (int i=0; i<nums.size(); i++){
-            mp[preSum]++;
+            // mp[preSum]++; 
             preSum += nums[i];
 
             int rem = preSum - k;
@@ -12,6 +13,8 @@ public:
             if (mp.find(rem) != mp.end()){
                 cnt+=mp[rem];
             }
+
+            mp[preSum]++;
         }
         return cnt;
     }
