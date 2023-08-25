30d0d0afb88d2658dec05a404e512e5b3d76cbfd
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-25 21:20:57 +0530

    Time: 503 ms (69.06%), Space: 173.4 MB (66.23%) - LeetHub

---
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int timer;
+    void dfs(vector<int> adj[], int node, int parent, vector<int> &vis, int tin[], int low[],
+            vector<vector<int>> &bridges){
+        vis[node] = 1;
+        
+        tin[node] = low[node] = timer;
+        timer++;
+
+        for (auto it : adj[node]){
+            if (parent == it) continue;
+            if (!vis[it]){
+                dfs(adj,it,node,vis,tin,low,bridges);
+                low[node] = min(low[node], low[it]);  // UPDATE THE LOW OF PARENT
+                
+                if (tin[node] < low[it]){              
+                    bridges.push_back({node,it});
+                } 
+            }
+            else{                                    
+                low[node] = min(low[node], low[it]);
+            }
+        }
+
+        return ;
+    }
+    vector<vector<int>> criticalConnections(int V, vector<vector<int>>& connections) {
+        // THIS IS GOING TO BE SOLVED USING TARJANS ALGORITHM 
+        // BRDIGE -> AN EDGE WHOSE REMOVAL DIVIDES INTO 2 OR MORE COMPONENTS!
+        // WHICH IS NOTHING BUT A DFS WITH FEW VARIATIONS 
+
+        // HAR EK NODE A KA LOW[] AND INSERTION TIME[]
+        // INSERTION TIME -> KITNE STEPS LAGENGE USS NODE TAK PAHONCHNE MEIN AND
+        
+        // LOW[] -> AT MIN MEIN KIS NODE TAK PAHONCH SKTA HOON WHICH ARE MY CHILDREN ONLY!
+        // LOW[] -> LOWEST INSERTION TIME OF ADJACENT NODES APART FROM PARENT!
+        
+
+        // JAB AAP KISI NODE PE AUR AAGE NHI JAA SKTE THEN PICK UP THE LOW OF THAT NODE'S CHILDREN
+        // JAB WAPAS AATE HAIN PARENT NODE PE TOH AGAIN PARENT KA LOW CHANGE KR DO
+        // AND NOW CHECK WHETER THIS EDGE B/W PARENT AND CHILD CAN BE A BRIDGE OR NOT 
+        // IF (LOW[CHILD] <= INSERTION TIME[PARENT]) -> NO BRIDGE
+        // ELSE  FOR BRIDGE -> LOW[CHILD] > INSERTION TIME[PARENT]
+
+
+        vector<int> adj[V];
+        for (auto it : connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        int tin[V] , low[V] ;
+        vector<int> vis(V,0);
+        vector<vector<int>> bridges;
+        dfs(adj,0,-1,vis,tin,low, bridges);
+
+        return bridges;
+
+    }
+};
