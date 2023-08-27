ba8f4e007c90c3083ebf7be00687e7ba7b89e25d
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-27 12:37:45 +0530

    Time: 3 ms (89.94%), Space: 7.5 MB (47.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,56 +1,23 @@
 class Solution {
 public:
-
-    bool valid(char ch) {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-            return 1;
-        } 
-        return 0;
+    bool checkPallindrome(string& arr, int i, int j){
+        if (i >= j) return true;
+        if (arr[i]!=arr[j]) return false;
+        return checkPallindrome(arr,i+1,j-1);   
     }
-    
-    char toLowerCase(char ch) {
-        if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') ){
-            return ch;
-        }
-        else{
-            char temp = ch - 'A' + 'a';
-            return temp;
-        }
-    }
-    bool checkPalindrome(string a) {
-        int s = 0;
-        int e = a.length()-1;
-
-        while(s<=e) {
-            if(a[s] != a[e])
-            {
-                return 0;       
-            }
-            else{
-                s++;
-                e--;
-            }
-        }
-        return 1;
-    }
-
     bool isPalindrome(string s) {
-        //faltu character hatado
-        string temp = "";
-        
-        for(int j=0; j<s.length(); j++) {   
-            if(valid(s[j])) {
-                temp.push_back(s[j]);
+        string ans;
+        for (char ch : s){
+            if (ch >= 'A' && ch<='Z'){
+                char var = char(ch + 32);
+                ans.push_back(var);
             }
+            else if (ch >= 'a' && ch<='z' || ch>='0' && ch<='9') ans.push_back(ch);
         }
         
-        //lowercase me kardo
-        for(int j=0; j<temp.length(); j++) { 
-            temp[j] = toLowerCase(temp[j]);
-        }
+        if (ans.size() == 0) return true;
         
-        //check palindrome
-        return checkPalindrome(temp);
+        return checkPallindrome(ans,0,ans.size()-1);
         
-    }   
+    }
 };
