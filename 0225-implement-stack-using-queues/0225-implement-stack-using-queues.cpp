1085db73ade07b51deccd35404af8a498afd7f40
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-28 10:09:18 +0530

    Time: 3 ms (36.88%), Space: 6.7 MB (67.22%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -2,30 +2,28 @@ class MyStack {
 public:
     queue<int> q;
     MyStack() {
-        
     }
     
     void push(int x) {
-        // ADD INTO QUEUE
-        // THEN TRANSFER ALL THE ELEMENTS FROM THE FRONT TO THE BACK 
+        if (q.empty()){
+            q.push(x);
+            return ;
+        }
         q.push(x);
-
-        int size = q.size()-1;
-
-        for (int i=0; i<size; i++){
+        int size = q.size();
+        for (int i=0; i<size-1; i++){
             int x = q.front();
             q.pop();
             q.push(x);
         }
 
-        return; 
+        return ;
     }
     
     int pop() {
         int x = q.front();
         q.pop();
         return x;
-
     }
     
     int top() {
@@ -33,8 +31,7 @@ public:
     }
     
     bool empty() {
-        if (!q.empty()) return false;
-        return true;
+        return q.empty();
     }
 };
 
