a0861622d53a8f8d3410a215a6323d6d7888d051
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-31 14:08:20 +0530

    Time: 77 ms (26.87%), Space: 34.4 MB (29.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& grid,int row, int col, int n, int m, vector<vector<int>>& vis){
+    int n,m;
+    bool isValid(vector<vector<int>>& grid,int row, int col, vector<vector<int>>& vis){
         if (row>=0 && row<n && col>=0 && col < m && grid[row][col] == 1 && vis[row][col]==0){
             vis[row][col] = 1;
             return true;
@@ -9,46 +10,41 @@ public:
     }
 
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+        n = grid.size(), m = grid[0].size();
         
         vector<vector<int>> vis(n, vector<int>(m,0));
         vector<vector<int>> ans(n, vector<int>(m,0));
         
-        queue<pair<int,int>> q;
+        queue<pair<int,pair<int,int>>> q;
         
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
                 if (grid[i][j] == 0){
-                    q.push({i,j});
+                    q.push({0,{i,j}});
                     vis[i][j] = 1;
                     ans[i][j] = 0;
                 }
             }
         }
 
-        int dis = 1;
         while (!q.empty()){
-            int size = q.size();
-          
-            for (int i=0; i<size; i++){
-                int row = q.front().first;
-                int col = q.front().second;
-                q.pop();
-                
-                int delrow[4] = {-1,0,1,0};
-                int delcol[4] = {0,1,0,-1};
-            
-                for (int j=0; j<4; j++){
-                    int nrow = delrow[j] + row;
-                    int ncol = delcol[j] + col;
+            auto node = q.front(); q.pop();
 
-                    if (isValid(grid,nrow,ncol,n,m,vis)){
-                        ans[nrow][ncol] = dis;
-                        q.push({nrow,ncol});
-                    }
+            int dis = node.first, row = node.second.first, col = node.second.second;
+            
+            int delrow[4] = {-1,0,1,0};
+            int delcol[4] = {0,1,0,-1};
+        
+            for (int i=0; i<4; i++){
+                int nrow = delrow[i] + row;
+                int ncol = delcol[i] + col;
+
+                if (isValid(grid,nrow,ncol,vis)){
+                    ans[nrow][ncol] = dis + 1;
+                    q.push({dis+1,{nrow,ncol}});
                 }
             }
-            dis++;
+          
         }
 
         return ans;
