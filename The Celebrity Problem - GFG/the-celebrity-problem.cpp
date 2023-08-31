8dc4bcd77a81d5e4b94e933dbe9a079f9ce1bef3
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-08-31 19:57:55 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -11,41 +11,35 @@ class Solution
 {
     public:
     //Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& M, int n) {
-        // code here 
-        int row = M.size();
-        int col = M[0].size();
-        int x = -1;
-        for (int i=0; i<row; i++){
-            int flagRow = 1, flagCol = 1; // CONSIDERING THE CELEBRITY
-            // CHECK ITS ROWS -> ALL BE 0's
-            for (int j=0; j<col; j++){
-                if (M[i][j]!=0 && i!=j){
-                    flagRow = 0;
-                    break;
+    int celebrity(vector<vector<int>>& grid, int V) {
+        // WE ARE GOING TO CREATE THE ADJACENCY LIST AND WILL DO THIS QUESTION USING INDEGREE AND OUTDEGREE CONCEPT SIMILAR TO 
+        
+        //                                            TOWN JUDGE
+
+        vector<int> adj[V];
+        int n = grid.size(), m = grid[0].size();
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                if (grid[i][j] == 1){
+                    adj[i].push_back(j);
                 }
             }
-            
-            if (flagRow == 0) continue;
-            
-            //CHECK ITS COL -> ALL 1'S
-            for (int j=0; j<row; j++){
-                // if (i == j) continue;
-                if (i!=j && M[j][i]!=1){
-                    flagCol = 0;
-                    break;
-                }
-            }
-            
-            if (flagRow == 0 || flagCol == 0) return -1;
-            
-            if (flagRow == 1 && flagCol == 1) x = i;
         }
         
-        return x;
+        vector<int> inDegree(n,0);
+        vector<int> outDegree(n,0);
+        for (int i=0; i<n; i++){
+            for (auto it : adj[i]){
+                inDegree[it]++, outDegree[i]++;
+            }
+        }
+        for (int i=0; i<n; i++){
+            if (inDegree[i] == n-1 && outDegree[i] == 0) return i;
+        }
+        
+        return  -1;
     }
 };
-
 
 //{ Driver Code Starts.
 
