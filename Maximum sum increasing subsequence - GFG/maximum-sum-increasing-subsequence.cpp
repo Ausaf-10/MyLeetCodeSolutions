b6168992e2ca8eea881b448f78594575d525eb90
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-09-07 20:28:33 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -7,28 +7,54 @@ class Solution{
 		
 
 	public:
+	
+	int recursion(int indx, int prevIndx, int arr[], int n){
+	       if (indx == n) return 0;
+	       
+	       int len = 0 + recursion(indx+1, prevIndx, arr, n);
+	       
+	       if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
+	           len = max(arr[indx] + recursion(indx + 1, indx, arr, n), len);
+	       }
+	       
+	       return len;
+	}
+	
+	int memoizatoin(int indx, int prevIndx, int arr[], int n, vector<vector<int>> &dp){
+	    if (indx == n) return 0;
+	    if (dp[indx][prevIndx + 1]!=-1) return  dp[indx][prevIndx + 1];
+	    
+	    int len = 0 + memoizatoin(indx+1, prevIndx, arr, n, dp);
+       
+        if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
+            len = max(arr[indx] + memoizatoin(indx + 1, indx, arr, n, dp), len);
+        }
+       
+        return dp[indx][prevIndx + 1] = len;
+        
+	}
+	
 	int maxSumIS(int arr[], int n)  {  
 	    // Your code goes here
-	    vector<int> temp;
-	    vector<int> dp(n,1), hash(n,0);
-	    for (int i=0; i<n; i++) dp[i] = arr[i];
+	   // vector<int> dp(n,1);
+	   // for (int i=0; i<n; i++) dp[i] = arr[i];
 	    
-	    int lastIndx = -1, maxLen = -1;
-	    for (int i=0; i<n; i++){
-	        for(int prev = 0; prev<i; prev++){
-	            if (arr[i] > arr[prev] && dp[prev] + arr[i] > dp[i]){
-                    dp[i] = dp[prev] + arr[i];
-                    hash[i] = prev;
-	            }
-	        }
-	    }
+	   // int lastIndx = -1, maxLen = -1;
+	   // for (int i=0; i<n; i++){
+	   //     for(int prev = 0; prev<i; prev++){
+	   //         if (arr[i] > arr[prev] && dp[prev] + arr[i] > dp[i]){
+    //                 dp[i] = dp[prev] + arr[i];
+    //                 hash[i] = prev;
+	   //         }
+	   //     }
+	   // }
 	    
-	    int maxi = -1e9;
-	    for (int i=0; i<n; i++){
-	        maxi = max(maxi, dp[i]);
-	    }
-	 
-	    return maxi;
+	   // int maxi = -1e9;
+	   // for (int i=0; i<n; i++){
+	   //     maxi = max(maxi, dp[i]);
+	   // }
+	    vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
+	    return memoizatoin(0, -1, arr,n, dp);
 	}  
 };
 
