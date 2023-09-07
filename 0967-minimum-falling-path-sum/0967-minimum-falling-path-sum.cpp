5836a33be233064e16b75c8d004ea0d98c4819e4
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-09-07 16:47:43 +0530

    Time: 12 ms (74.83%), Space: 10.6 MB (50%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,75 +1,58 @@
 class Solution {
-public:
-    int recursion(int row, int col, vector<vector<int>>& grid, int n){
-        if (row == 0) return grid[row][col];
-        int up = INT_MAX , upperLeft = INT_MAX, upperRight = INT_MAX;
-        if (row > 0 ) up = recursion(row-1,col,grid,n);
-        if (col > 0) upperLeft = recursion(row-1,col-1,grid,n);
-        if (col < n-1) upperRight = recursion(row-1,col+1,grid,n);
-        return grid[row][col] + min(up,min(upperRight,upperLeft));
+public: 
+    int n,m;
+    // ROW: N-1->0, COL: M-1->0
+    int recursion(vector<vector<int>>& grid, int row, int col){
+        if (col < 0  || col >= m) return 1e9;
+        if (row == 0) return grid[0][col];
+
+        int up = recursion(grid, row-1, col) + grid[row][col];
+        int upLeft = recursion(grid,row-1,col-1) + grid[row][col];
+        int upRight = recursion(grid,row-1,col+1) + grid[row][col];
+       
+        return min(up,min(upLeft,upRight));  
     }
 
-    int memoization(int row, int col, vector<vector<int>>& grid, int n, vector<vector<int>>& dp ){
-        if (row == 0) return grid[row][col];
+    int memoization(vector<vector<int>>& grid, int row, int col, vector<vector<int>> &dp){
+        if (col < 0  || col >= m) return 1e9;
+        if (row == 0) return grid[0][col];
+
         if (dp[row][col]!=-1) return dp[row][col];
 
-        int up = INT_MAX , upperLeft = INT_MAX, upperRight = INT_MAX;
-        
-        if (row > 0 ) up = memoization(row-1,col,grid,n,dp);
-        if (col > 0) upperLeft = memoization(row-1,col-1,grid,n,dp);
-        if (col < n-1) upperRight = memoization(row-1,col+1,grid,n,dp);
-        
-        return dp[row][col] = grid[row][col] + min(up,min(upperRight,upperLeft));
+        int up = memoization(grid, row-1, col, dp) + grid[row][col];
+        int upLeft = memoization(grid,row-1,col-1, dp) + grid[row][col];
+        int upRight = memoization(grid,row-1,col+1, dp) + grid[row][col];
+       
+        return dp[row][col] = min(up,min(upLeft,upRight));  
     }
-    
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int mini = INT_MAX;
-        // vector<vector<int>> memoizationDP(n, vector<int>(n,-1));
 
-        // vector<vector<int>> tabulationDP(n, vector<int>(n,0));
-        // for (int j=0; j<n; j++){
-        //     tabulationDP[0][j] = matrix[0][j];
-        // }
-
-        // for (int row=1; row<n; row++){
-        //     for (int col = 0; col < n; col++){
-        //         int up = INT_MAX , upperLeft = INT_MAX, upperRight = INT_MAX;
-            
-        //         if (row > 0 ) up = tabulationDP[row-1][col];
-        //         if (col > 0) upperLeft = tabulationDP[row-1][col-1];
-        //         if (col < n-1) upperRight = tabulationDP[row-1][col+1];
-        //         tabulationDP[row][col] = matrix[row][col] + min(up,min(upperRight,upperLeft));
-        //     }
-            
-        // }
-
-        vector<int> prev(n,0);
-        for (int j = 0; j < n; j++){
-            prev[j] = matrix[0][j];
+    int tabulation(vector<vector<int>>& grid){
+        int mini = 1e9;
+        vector<vector<int>> tabulationDP(n, vector<int>(n,0));
+        for (int j=0; j<m; j++){
+            tabulationDP[0][j] = grid[0][j];
         }
-        for (int row = 1; row < n; row++){
-            vector<int> temp(n,0);
-            for (int col = 0; col < n; col++){
-                int upperLeft = INT_MAX, upperRight = INT_MAX;
-            
-                int up = prev[col];
-                if (col > 0) upperLeft = prev[col-1];
-                if (col < n-1) upperRight = prev[col+1];
-                temp[col] = matrix[row][col] + min(up,min(upperRight,upperLeft));
+        for (int row=1; row<n; row++){
+            for (int col=0; col<m; col++){
+                int up = tabulationDP[row-1][col];
+                int upperLeft = 1e9, upperRight = 1e9;
+                if (col > 0) upperLeft = tabulationDP[row-1][col-1];
+                if (col < n-1) upperRight = tabulationDP[row-1][col+1];
+                tabulationDP[row][col] = grid[row][col] + min(up,min(upperRight,upperLeft));
+                    
             }
-            prev = temp;
         }
-
-        for (int j= 0; j < n; j++){
-            mini = min(mini, prev[j]);   
+        for (int j=0; j<m; j++){
+            mini = min(mini, tabulationDP[n-1][j]);
         }
-
         return mini;
 
-        // for (int j=0; j<n; j++){
-        //     mini = min(mini,memoization(n-1,j,matrix,n,memoizationDP));
-        // }
-        // return mini;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        n = grid.size(), m = grid[0].size();
+        // int mini = 1e9;
+        return tabulation(grid);
+
     }
 };
