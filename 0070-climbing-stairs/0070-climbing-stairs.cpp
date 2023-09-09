67527570f6dcd82abb6c2703896ed2de6ed45b0b
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-09-09 12:03:57 +0530

    Time: 0 ms (100%), Space: 6.4 MB (5.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
+    // indx: n -> 0
     int recursion(int n){
-        if (n == 0) return 1;
-        int first = recursion(n-1);
-        int two = 0;
-        if (n > 1) two = recursion(n-2);
-        return first + two;
+        if (n <= 1) return 1;
+        return recursion(n-1) + recursion(n-2);
     }
 
     int memoization(int n, vector<int>& dp){
-        if (n == 0) return 1;
+        if (n <= 1) return 1;
         if (dp[n]!=-1) return dp[n];
-        int first = memoization(n-1,dp);
-        int two = 0;
-        if (n > 1) two = memoization(n-2,dp);
-        return dp[n] = first + two;
+        return memoization(n-1, dp) + memoization(n-2, dp);
+    }
+    int tabulation(int n){
+        vector<int> dp(n+1,-1);
+        dp[0] = 1, dp[1] = 1;
+        for (int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
-        // vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,-1);
         // dp[0] = 1;
         // for (int i=1; i<=n; i++){
         //     int first = dp[i-1];
@@ -29,13 +32,15 @@ public:
 
         // return memoization(n,dp);
 
-        int prev  = 1, prev2 = 0;
+        // int prev  = 1, prev2 = 0;
         
-        for (int i=1; i<=n ; i++){
-            int cur = prev + prev2;
-            prev2 = prev , prev = cur;
-        }
+        // for (int i=1; i<=n ; i++){
+        //     int cur = prev + prev2;
+        //     prev2 = prev , prev = cur;
+        // }
 
-        return prev;
+        // return prev;
+
+        return tabulation(n);
     }
 };
