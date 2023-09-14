993e8044f6890745e282ed35af1ab095a4a50a9b
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2023-09-14 20:50:32 +0530

    Time: 181 ms (68.37%), Space: 126.3 MB (51.8%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int V;
-    void dfs(vector<int> adj[], int node, vector<int>& vis, vector<int>& path, vector<vector<int>>& ans){
-        vis[node] = 1, path.push_back(node);
+    void dfs(vector<int> adj[], int node, vector<int>& vis, int& numberOfVertices, int& numberOfEdges){
+        vis[node] = 1, numberOfVertices+=1, numberOfEdges+=adj[node].size();
+
         for (auto it : adj[node]){
             if (!vis[it]){
-                dfs(adj, it, vis, path, ans);
+                dfs(adj, it, vis, numberOfVertices, numberOfEdges);
             }
         }
         return ;
@@ -22,28 +23,15 @@ public:
         vector<int> vis(V,0);
 
         vector<vector<int>> ans;
+        int cnt = 0;
         for (int i=0; i<V; i++){
             if (!vis[i]){
-                vector<int> path;
-                dfs(adj, i, vis, path, ans);
-                ans.push_back(path);
+                int numberOfVertices = 0, numberOfEdges = 0;
+                dfs(adj, i, vis, numberOfVertices, numberOfEdges);
+                numberOfEdges/=2;
+                int val = (numberOfVertices*(numberOfVertices-1))/2;
+                if (numberOfEdges == val) cnt++;
             }
-        }
-        // ans = {{0,1,2},{3,4},{5}}
-        int cnt = 0;
-        for (auto it : ans){
-            if (it.size() == 1){
-                cnt++;
-                continue;
-            }
-            int numberOfVertices = it.size();
-            int numberOfEdges = 0;
-            for (auto j : it){
-                numberOfEdges+=adj[j].size();
-            }
-            numberOfEdges/=2;
-            int val = (numberOfVertices*(numberOfVertices-1))/2;
-            if (numberOfEdges == val) cnt++;
         }
         return cnt;
     }
