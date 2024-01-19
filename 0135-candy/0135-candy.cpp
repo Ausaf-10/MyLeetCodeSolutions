7dafdbe57e4654b5becfb5998c63e7fdfca2c447
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-01-19 21:24:33 +0530

    Time: 10 ms (96.98%), Space: 21.2 MB (5.07%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,23 +3,25 @@ public:
     int candy(vector<int>& arr) {
         int n = arr.size();
         vector<int> l2r(n,1), r2l(n,1);
+        
+        vector<int> count(n,1);
 
         for (int i=1; i<n; i++){
             if (arr[i] > arr[i-1]){
-                l2r[i] = l2r[i-1]+1;
+                count[i] = count[i-1]+1;
             }
         }
         
         for (int i=n-2; i>=0; i--){
             if (arr[i] > arr[i+1]){
-                r2l[i] = r2l[i+1]+1;
+                count[i] = max(count[i], count[i+1]+1);
             }
         }
         
         int cnt = 0;
         
         for (int i=0; i<n; i++){
-            cnt+=max(l2r[i], r2l[i]);
+            cnt+=count[i];
         }
         
         return cnt;
