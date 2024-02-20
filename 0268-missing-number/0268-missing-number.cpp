a7d250d1d2a25011be77da3a8c4af0114b5c2eb2
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-02-20 13:05:42 +0530

    Time: 15 ms (58.96%), Space: 20.3 MB (47.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,11 +3,9 @@ public:
     int missingNumber(vector<int>& arr) {
         int n = arr.size();
         int xr1 = 0, xr2 = 0;
-        for (int number : arr){
-            xr1 = xr1^number;
-        }
-        for (int i=0; i<=n; i++){
-            xr2 = xr2^i;
+        for (int i=0; i<n; i++){
+            xr1 = xr1^arr[i];
+            xr2 = xr2^(i+1);
         }
         return xr1^xr2;
     }
