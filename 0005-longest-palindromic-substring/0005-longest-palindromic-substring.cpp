b229cf17392c5809710d94f9fea33761fff0e50c
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-04-23 19:16:08 +0530

    Time: 307 ms (22.67%), Space: 290 MB (12.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,51 +1,24 @@
 class Solution {
 public:
+    bool checkPallindrome(string& str, int i, int j, vector<vector<int>>& dp){
+        if (i > j) return true;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (str[i]!=str[j]) return dp[i][j] = false;
+        return dp[i][j] = checkPallindrome(str, i+1, j-1, dp);
+    }
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n <= 1) return s;
-
-        int st = 0, end = 0;
-        int maxLen = 1;
-       
-       // ODD LENGTH PALLINDROME!!!
-        for (int i=0; i<n-1; i++){
-            int l=i, r = i;
-            while (l>=0 && r < n){
-                if (s[l] == s[r]){
-                    l--,r++;
+        int n = s.size(), maxi = 0, start = 0;
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        for (int i=0; i<n; i++){
+            for (int j=i; j<n; j++){
+                if (checkPallindrome(s,i,j, dp)){
+                    if ( j-i+1 > maxi){
+                        maxi = j-i+1;
+                        start = i;
+                    }
                 }
-                else break;
-            }
-            int len = r - l - 1;
-            if (len > maxLen){
-                maxLen = len;
-                st = l+1, end = r-1;
             }
         }
-
-        for (int i=0; i<n-1; i++){
-            int l=i, r = i+1;
-            while (l>=0 && r < n){
-                if (s[l] == s[r]){
-                    l--,r++;
-                }
-                else break;
-            }
-            int len = r - l - 1;
-            if (len > maxLen){
-                maxLen = len;
-                st = l+1, end = r-1;
-            }
-        }
-        
-
-        string ans(end-st+1, ' ');
-        int indx = 0;
-        for (int i=st; i<=end; i++){
-            ans[indx] = s[i];
-            indx++;
-        }
-
-        return ans;
+        return s.substr(start, maxi);
     }
 };
