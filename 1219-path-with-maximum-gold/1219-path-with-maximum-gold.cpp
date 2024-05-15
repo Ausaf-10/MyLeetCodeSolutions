8374639bfeee784e7902528f0ba3296447aa67a5
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-05-15 11:13:58 +0530

    Time: 139 ms (57.18%), Space: 9.2 MB (63.91%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int n,m;
-    bool isValid(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid){
-        if (row>=0 && row<n && col>=0 && col<m && !vis[row][col] && grid[row][col])  return true;
-        return false;
-    }
-    void fun(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis, int &maxi, int& sum){
-        sum+=grid[row][col];
-        vis[row][col] = 1;
-        int delrow[4] = {-1, 0, 1, 0};
-        int delcol[4] = {0, 1, 0, -1};
+    vector<vector<int>> directions{{-1,0},{0,1},{1,0},{0,-1}};
+    int DFS(int row, int col, vector<vector<int>>& grid){
+        if (row < 0 || row>=n || col<0 || col>=m || grid[row][col] == 0) return 0;
+        int originalValue = grid[row][col];
+        grid[row][col] = 0;
+
+        int maxi = 0;
         for (int i=0; i<4; i++){
-            int nrow = delrow[i] + row;
-            int ncol = delcol[i] + col;
-            if (isValid(nrow, ncol, vis, grid)){
-                fun(nrow, ncol, grid, vis, maxi, sum);
-            }
+            int nrow = row + directions[i][0];
+            int ncol = col + directions[i][1];
+            maxi = max(maxi, DFS(nrow, ncol, grid));
         }
-        maxi = max(maxi, sum);
-        sum-=grid[row][col];
-        vis[row][col] = 0;
-        return ;
+
+        grid[row][col] = originalValue;
+        return maxi + originalValue;
     }
     int getMaximumGold(vector<vector<int>>& grid) {
         n = grid.size(), m = grid[0].size();
-        
-        int maxi = -1;
+        int maxi = 0;
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
-                vector<vector<int>> vis(n, vector<int>(m,0));
-                if (grid[i][j] == 0) continue;
-                int sum = 0;
-                fun(i, j, grid, vis, maxi, sum);
+                if (!grid[i][j]) continue;
+                maxi = max(maxi, DFS(i,j,grid));
             }
         }
-        return maxi == -1 ? 0 : maxi;
+
+        return maxi;
     }
 };
