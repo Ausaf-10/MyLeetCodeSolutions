1a28e9fa2565faf6f8043b34a272b8ed8c0b7d65
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-05-31 21:50:18 +0530

    Time: 0 ms (100%), Space: 7.2 MB (89.9%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -5,29 +5,37 @@ public:
         int left = 0, right = 0, n = version1.size(), m = version2.size();
         while (left<n && right<m){
             int i = left, j = right;
-            while (i<n && version1[i]!='.') i++;
-            while (j<m && version2[j]!='.') j++;
-            string s1 = version1.substr(left, i-left); 
-            string s2 = version2.substr(right, j-right);
-            int num1 = stoi(s1), num2 = stoi(s2);
+            int num1 = 0, num2 = 0;
+            while (i<n && version1[i]!='.'){
+                num1 += num1*1ll*10 + (version1[i]-'0');
+                i++;
+            }
+            while (j<m && version2[j]!='.') {
+                num2 += num2*1ll*10 + (version2[j]-'0');
+                j++;
+            }
             if (num1 > num2) return 1;
             else if (num1 < num2) return -1;
             left=i+1, right = j+1;  
         }
         while (left < n){
             int i = left;
-            while (i<n && version1[i]!='.') i++;
-            string s1 = version1.substr(left, i-left);
-            int num1 = stoi(s1);
+            int num1 = 0;
+            while (i<n && version1[i]!='.'){
+                num1 += num1*1ll*10 + (version1[i]-'0');
+                i++;
+            }
             if (num1 > 0) return 1;
             else if (num1 < 0) return -1;
             left = i+1;
         }
         while (right < m){
             int j = right;
-            while (j<m && version2[j]!='.') j++;
-            string s2 = version2.substr(right, j-right);
-            int num2 = stoi(s2);
+            int num2 = 0;
+            while (j<m && version2[j]!='.') {
+                num2 += num2*1ll*10 + (version2[j]-'0');
+                j++;
+            }
             if (num2 > 0) return -1;
             else if (num2 < 0) return 1;
             right = j+1;
