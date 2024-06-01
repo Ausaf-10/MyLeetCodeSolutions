fe388d398068ab81b9cfdad82a0a718457e40261
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-01 16:27:07 +0530

    Time: 2 ms (54.34%), Space: 10.8 MB (24.93%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
-        int ans = 0;
-        for (int i=citations.size(); i>=0; i--){
-            int cnt = 0;
-            for (auto it : citations){
-                if (it >= i) cnt++;
-            }
-            if (cnt >= i) return i;
+    bool check(vector<int>& citations, int h){
+        int cnt = 0;
+        for (auto it : citations){
+            if (it >= h) cnt++;
         }
-        return -1;
+        if (cnt >= h) return true;
+        return false;
+    }
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(), low = 0, high = n;
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            if (check(citations, mid)) low = mid + 1;
+            else high = mid - 1;
+        } 
+        return high;
     }
 };
