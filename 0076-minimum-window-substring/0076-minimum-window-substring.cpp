9bc4c44dfd5ea8070fbcee4d71e220660c74461a
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-06 01:45:39 +0530

    Time: 29 ms (33.85%), Space: 10.9 MB (17.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int> mp;
-        for (char ch : t) mp[ch]++;
-        int cnt = mp.size();
-        string ans;
-        int n = s.size(), mini = 1e9, indx = -1;
-        int i=0,j=0;
-
-        while (j < n){
+        int i = 0, j = 0, n = s.size(), mini = 1e9, startIndx = -1;
+        string ans = s, temp = "";
+        unordered_map<int, int> mp;
+        for (auto it : t) mp[it]++;
+        int cnt = mp.size(); 
+        while (j<n){
             if (mp.find(s[j]) != mp.end()){
                 mp[s[j]]--;
-                if (mp[s[j]]== 0) cnt--;
+                if (mp[s[j]] == 0) cnt--;
             }
-            while (cnt == 0){
+            while (cnt == 0 && i<=j){
                 if (j-i+1 < mini){
-                    mini = j-i+1; 
-                    indx = i;
-                    // ans = s.substr(i,j-i+1);
+                    mini = j-i+1;
+                    startIndx = i;
                 }
                 if (mp.find(s[i]) != mp.end()){
+                    if (mp[s[i]] == 0) cnt++;
                     mp[s[i]]++;
-                    if (mp[s[i]] > 0) cnt++;
                 }
                 i++;
-            } 
+            }
             j++;
         }
-        return indx == -1 ? "" : s.substr(indx, mini);
+        return mini == 1e9 ? "" : s.substr(startIndx, mini);
     }
 };
