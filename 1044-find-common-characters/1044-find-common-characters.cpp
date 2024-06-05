bf4aafc83987c75ffa5acfad34cab3828f35688c
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-05 17:22:39 +0530

    Time: 104 ms (5.38%), Space: 40.4 MB (5.1%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        unordered_map<char, int> characterCount;
+        unordered_map<string, int> wordCount;
+        int n = words.size();
+        for (auto it : words){
+            wordCount[it]++;
+            for (int i=0; i<it.size(); i++){
+                characterCount[it[i]]++;
+            }
+        }
+        vector<string> ans;
+        for (auto &it : characterCount){
+            int mini = 1e9, flag = 1, freq = it.second;
+            char ch = it.first;
+            for (auto &j: wordCount){
+                unordered_map<char, int> mp;
+                string str = j.first;
+                for (int i=0; i<str.size(); i++) mp[str[i]]++;
+                if (mp.find(ch) == mp.end()){
+                    flag = 0;
+                    break;
+                }
+                else{
+                    mini = min(mini, mp[ch]);
+                }
+            }
+            if (flag == 0) continue;
+            string temp;
+            temp.push_back(ch);
+            for (int i=0; i<mini; i++){
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
