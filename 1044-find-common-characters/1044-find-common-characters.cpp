010fa791bb6598d3a3f7a2c005d6e176e59819a7
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-05 18:20:26 +0530

    Time: 7 ms (62.84%), Space: 11.6 MB (51.34%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        unordered_map<char, int> characterCount;
-        int n = words.size();
-        for (auto it : words){
-            for (int i=0; i<it.size(); i++){
-                characterCount[it[i]]++;
+        vector<int> count(26, 0);
+        for (char ch: words[0]) count[ch-'a']++;
+        for (int i=1; i<words.size(); i++){
+            vector<int> temp(26,0);
+            for (int j=0; j<words[i].size(); j++){
+                temp[words[i][j]-'a']++;
+            }
+            for (int j=0; j<26; j++){
+                count[j] = min(count[j], temp[j]);
             }
         }
         vector<string> ans;
-        for (auto &it : characterCount){
-            int mini = 1e9, flag = 1, freq = it.second;
-            char ch = it.first;
-            for (int j=0; j<n; j++){
-                unordered_map<char, int> mp;
-                string str = words[j];
-                for (int i=0; i<str.size(); i++) mp[str[i]]++;
-                if (mp.find(ch) == mp.end()){
-                    flag = 0;
-                    break;
-                }
-                else{
-                    mini = min(mini, mp[ch]);
-                }
-            }
-            if (flag == 0) continue;
+        for (int i=0; i<26; i++){
+            if (!count[i]) continue;
             string temp;
-            temp.push_back(ch);
-            for (int i=0; i<mini; i++){
+            temp.push_back(i+'a');
+            for (int j=0; j<count[i]; j++){
                 ans.push_back(temp);
             }
         }
