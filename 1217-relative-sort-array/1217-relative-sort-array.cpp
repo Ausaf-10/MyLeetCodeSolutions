a85bef97e6130443d8c4304eb76c3dfb6838db5b
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-11 10:49:49 +0530

    Time: 4 ms (39.97%), Space: 9.6 MB (71.58%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int, int> mp;
+        int maxi = *max_element(arr1.begin(), arr1.end());
+        vector<int> arr(maxi+1, 0);
+        for (int i=0; i<arr1.size(); i++){
+            arr[arr1[i]]++;
+        }
         vector<int> ans;
-        for (auto it : arr1) mp[it]++;
         for (int i=0; i<arr2.size(); i++){
-            int freq = mp[arr2[i]];
+            int freq = arr[arr2[i]];
             for (int j=0; j<freq; j++){
                 ans.push_back(arr2[i]);
             }
-            mp.erase(arr2[i]);
+            arr[arr2[i]] = 0;
         }
-        for (auto it : mp){
-            int freq = it.second;
-            for (int j=0; j<freq; j++){
-                ans.push_back(it.first);
+        for (int i=0; i<maxi+1; i++){
+            if (arr[i]!=0){
+                int freq = arr[i];
+                for (int j=0; j<freq; j++){
+                    ans.push_back(i);
+                }
             }
         }
         return ans;
