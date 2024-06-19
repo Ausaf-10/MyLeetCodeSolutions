87091bcfa5c67c8618dae87f8b7d545595c9f090
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-19 12:33:05 +0530

    Time: 0 ms (100%), Space: 16.1 MB (49.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     int n;
-    int lowerBound(vector<int> arr, int x) {
-	// Write your code here
+    int firstOccurence(vector<int>& arr, int target){
         int low = 0, high = n-1;
-        int ans = n;
         while (low <= high){
-            int mid = low + (high - low)/2;
-            if (arr[mid] >= x){
-                high = mid - 1;
-                ans = mid;
-            }
+            int mid = low + (high-low)/2;
+            if (arr[mid] >= target) high = mid - 1;
             else low = mid + 1;
         }
-        return ans;
+        return high+1;
     }
-    int upperBound(vector<int> &arr, int x){
-        // Write your code here.	
+    int lastOccurence(vector<int>& arr, int target){
         int low = 0, high = n-1;
-        int ans = n;
         while (low <= high){
-            int mid = low + (high - low)/2;
-            if (arr[mid] > x){
-                high = mid - 1;
-                ans = mid;
-            }
-            else low = mid + 1;
+            int mid = low + (high-low)/2;
+            if (arr[mid] <= target) low = mid + 1;
+            else high = mid - 1;
         }
-        return ans;
+        return low - 1;
     }
-    vector<int> searchRange(vector<int>& nums, int target) {
-        n = nums.size();
-        int lb = lowerBound(nums, target); 
-        int ub = upperBound(nums, target);
-
-        if (lb == n || nums[lb]!= target) return {-1,-1};
-
-        return {lb, ub-1}; 
+    vector<int> searchRange(vector<int>& arr, int target) {
+        n = arr.size();
+        int first = firstOccurence(arr, target);
+        if (first == n || arr[first]!=target) return {-1,-1};
+        int second = lastOccurence(arr, target);   
+        return {first, second};
     }
 };
