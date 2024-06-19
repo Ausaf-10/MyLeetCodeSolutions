74e0ca0b018b783d16aac57ca00d3564c5ab4b3f
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-19 22:37:55 +0530

    Time: 55 ms (5.03%), Space: 19.4 MB (100%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,25 +1,25 @@
 using ll = long long int;
 class Solution {
 public:
-    ll fun(vector<int>& arr, int hour){
-        long long totTime = 0;
-        for (int i=0; i<arr.size(); i++){
-            totTime += ceil((double)arr[i]/(double)hour);
+    ll isPossible(vector<int>& arr, ll number){
+        ll hoursRequired = 0;
+        for (ll i=0; i<arr.size(); i++){
+            hoursRequired =  hoursRequired + ceil(arr[i]/(double)number);
         }
-        return totTime;
+        return hoursRequired;
     }
-    int minEatingSpeed(vector<int>& arr, int h){
+    int minEatingSpeed(vector<int>& arr, int h) {
         ll maxi = *max_element(arr.begin(), arr.end());
         ll low = 1, high = maxi;
         while (low <= high){
             ll mid = low + (high - low)/2;
-            // CHECK WHETHER IT IS POSSIBLE OR NOT
-            ll reqTime = fun(arr,mid);
-            if (reqTime<=h){
+            ll totalHoursTaken = isPossible(arr,mid);
+            if (totalHoursTaken <= (long long)h){
                 high = mid - 1;
             }
             else low = mid + 1;
         }
+        
         return low;
     }
 };
