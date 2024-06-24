98e01f56842072c4842df361ccdea493ff97a554
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-24 22:59:32 +0530

    Time: 0 ms (100%), Space: 7 MB (67.21%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        if (n == 0) return 0;
+        int ans = 0;
+        for (int i=1; i*i<=n; i++){
+            ans++;
+        }
+        return ans;
     }
 };
