c4c90f46ab1b5ab613579811d13628dcc01332b3
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-25 17:31:52 +0530

    Time: 326 ms (10.88%), Space: 189.4 MB (9.73%) - LeetHub

--- 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.cpp
@@ -28,41 +28,18 @@ public:
         ll pick = -1e9, notPick = -1e9;
         if (sign){ // positive sign hai peeche 
             pick = -arr[indx] + memoization(arr, indx+1, prev, !sign, dp);
-            notPick = arr[indx] + memoization(arr, indx+1, prev, sign, dp);
+            notPick = arr[indx] + memoization(arr, indx+1, prev, 1, dp);
         }
         if (!sign){
             pick = arr[indx] + memoization(arr, indx+1, prev, !sign, dp);
-            notPick = arr[indx] + memoization(arr, indx+1, prev, sign, dp);
+            notPick = arr[indx] + memoization(arr, indx+1, prev, 1, dp);
         }
         
         return dp[indx][prev][sign] = max(pick, notPick); 
     }
-    ll tabulation(vector<int>& arr){
-        vector<vector<vector<ll>>> dp(n+1, vector<vector<ll>>(2, vector<ll>(2,0)));
-        for (int indx=n-1; indx>=0; indx--){
-            for (int prev=0; prev<=1; prev++){
-                for (int sign=0; sign<=1; sign++){
-                    if (prev == 0){
-                        dp[indx][prev][sign] = arr[indx] + dp[indx+1][!prev][sign];
-                    }
-                    ll pick = -1e9, notPick = -1e9;
-                    if (sign){ // positive sign hai peeche 
-                        pick = -arr[indx] + dp[indx+1][prev][!sign];
-                    }
-                    if (!sign){
-                        pick = arr[indx] + dp[indx+1][prev][!sign];
-                    }
-                    notPick = arr[indx] + dp[indx+1][prev][sign];
-                    dp[indx][prev][sign] = max(pick, notPick); 
-                }
-            }
-        }
-        return dp[0][0][0];
-    }
     long long maximumTotalCost(vector<int>& arr) {
         n = arr.size();   
-        // vector<vector<vector<ll>>> dp(n+1, vector<vector<ll>>(2, vector<ll>(2,-1)));
-        // return memoization(arr, 0, 0, 1, dp);
-        return tabulation(arr);
+        vector<vector<vector<ll>>> dp(n+1, vector<vector<ll>>(2, vector<ll>(2,-1)));
+        return memoization(arr, 0, 0, 1, dp);
     }
 };
