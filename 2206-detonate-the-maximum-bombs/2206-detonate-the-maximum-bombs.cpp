a785748bd2ffd62d01ff6b1e5bb54ea22d359683
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-27 23:44:58 +0530

    Time: 450 ms (7.1%), Space: 42.7 MB (25%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,31 @@
+using ll = long long int;
+class Solution {
+public: 
+    int n;
+    void dfs(int indx, int& cnt, vector<vector<int>>& bombs, unordered_set<int> &st, int &maxi){
+        st.insert(indx);
+        cnt++;
+        maxi=max(maxi, cnt);
+        for (int i=0; i<n; i++){
+            if (st.find(i) != st.end()) continue;
+            int x1 = bombs[indx][0], y1 = bombs[indx][1], x2 = bombs[i][0], y2 = bombs[i][1];
+            ll radius = bombs[indx][2], area = radius*radius;
+            ll x_dist = abs(x2-x1), y_dist = abs(y2-y1);
+            ll dist = x_dist*x_dist + y_dist*y_dist;
+            if (dist <= area){
+                dfs(i, cnt, bombs, st, maxi);
+            }
+        }
+        return ;
+    }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int maxi = 1;
+        n = bombs.size();
+        for (int i=0; i<n; i++){
+            unordered_set<int> st;
+            int cnt = 0;
+            dfs(i, cnt, bombs, st, maxi);
+        }
+        return maxi;
+    }
+};
