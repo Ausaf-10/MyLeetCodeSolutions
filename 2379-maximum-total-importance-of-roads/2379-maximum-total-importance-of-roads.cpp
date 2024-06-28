c66b0272781497817d296228c471b7b7b4a88bde
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-28 22:32:05 +0530

    Time: 364 ms (37.24%), Space: 157.2 MB (26.55%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -1,28 +1,25 @@
 using ll = long long int;
 class Solution {
 public:
+    static bool compare(pair<int,int>& a, pair<int,int>& b){
+        return a.second > b.second;
+    }
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        priority_queue<pair<int,int>> pq;
-        unordered_map<int, int> mp;
         vector<int> adj[n];
         for (auto it : roads){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
+        vector<pair<int,int>> deg(n);
         for (int i=0; i<n; i++){
-            int size = adj[i].size();
-            pq.push({size,i});
+            deg[i] = {i,adj[i].size()};
         }
-        int k = n;
-        while (!pq.empty()){
-            auto node = pq.top(); pq.pop();
-            mp[node.second] = k;
-            k--;
-        }
+        sort(deg.begin(), deg.end(), compare);
         ll s = 0;
-        for (int i=0; i<roads.size(); i++){
-            s+=mp[roads[i][0]];
-            s+=mp[roads[i][1]];
+        int k = n;
+        for (int i=0; i<n; i++){
+            s += (k*1ll*(deg[i].second));
+            k--;
         }
         return s;
     }
