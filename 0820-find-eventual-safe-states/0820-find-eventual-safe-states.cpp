9410c05a5f9291a7c674fcdacaf35121b65f28bb
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-06-29 19:10:47 +0530

    Time: 155 ms (19.92%), Space: 47.9 MB (100%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-    int V;
-    bool dfs(vector<int> adj[], int node, vector<int>& vis, vector<int>& pathVis, vector<int>& checkNode){
-        vis[node] = 1, pathVis[node] = 1;
-        for (auto neighbor : adj[node]){
-            if (!vis[neighbor]){
-                if (dfs(adj, neighbor, vis, pathVis, checkNode)) return true;
+    bool dfs(vector<vector<int>>& adj, int node, vector<int>& vis, vector<int>& pathVis, vector<int>& safeNodes){
+        vis[node] = 1 , pathVis[node] = 1;
+        
+        for (auto neighbour : adj[node]){
+            if (!vis[neighbour]) {
+                if (dfs(adj,neighbour,vis,pathVis,safeNodes)) return true;
             }
-            else if (pathVis[neighbor]) return true;
+            else if (pathVis[neighbour] == 1) return true;
         }
 
+        safeNodes.push_back(node);
         pathVis[node] = 0;
-        checkNode[node] = 1;
-
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        V = graph.size();
-        vector<int> adj[V];
-        for (int i=0; i<graph.size(); i++){
-            for (int j=0; j<graph[i].size(); j++){
-                adj[i].push_back(graph[i][j]);
-            }
-        }
-
-        vector<int>vis(V,0);
-        vector<int>pathVis(V,0);
-        vector<int>checkNode(V,0);
-
+        int V = graph.size();
+        
+        vector<int> vis(V,0);
+        vector<int> pathVis(V,0);
+        vector<int> safeNodes;
+        
         for (int i=0; i<V; i++){
             if (!vis[i]){
-                if (dfs(adj,i,vis,pathVis,checkNode));
+                if (dfs(graph,i,vis,pathVis,safeNodes));
             }
         }
 
-        vector<int> safeNodes;
-        for (int i=0; i<V; i++){
-            if (checkNode[i]) safeNodes.push_back(i);
-        }
+        sort(safeNodes.begin(),safeNodes.end());
+        
         return safeNodes;
     }
+
+
 };
