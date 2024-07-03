4ce464e32d7b2d274a916a398fd2fa17d6d03d86
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-03 19:39:11 +0530

    Time: 142 ms (85.23%), Space: 119.1 MB (68.85%) - LeetHub

--- 3413-find-the-first-player-to-win-k-games-in-a-row/3413-find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/3413-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -1,44 +1,14 @@
 class Solution {
 public:
-    int findWinningPlayer(vector<int>& skills, int k) {
-        int n = skills.size(), maxi = *max_element(skills.begin(), skills.end());
-        deque<int> dq;
-        for (int i=0; i<n; i++){
-            dq.push_back(i);
-        }
-        int ans = 0, indx = -1, cnt = 0;
-        while (true){
-            int first = dq.front(); dq.pop_front();
-            if (skills[first] == maxi) return first;
-            int second = dq.front(); dq.pop_front();
-            if (skills[first] > skills[second]){
-                dq.push_front(first);
-                dq.push_back(second);
-                if (indx == first){
-                    cnt++;
-                    if (cnt == k) return indx;
-                }
-                else{
-                    cnt = 1;
-                    indx = first;
-                    if (k == 1) return indx;
-                }
-                
+    int findWinningPlayer(vector<int>& arr, int k) {
+        int prev = 0, n = arr.size(), cnt = 0, i;
+        for (i=1; i<n; i++){
+            if (arr[i] > arr[prev]){
+                cnt = 0;
+                prev = i;
             }
-            else{
-                dq.push_front(second);
-                dq.push_back(first);
-                if (indx == second){
-                    cnt++;
-                    if (cnt == k) return indx;
-                }
-                else{
-                    cnt = 1;
-                    indx = second;
-                    if (k == 1) return indx;
-                }
-            }
+            if (++cnt == k) return prev;
         }
-        return -1;
+        return prev;
     }
 };
