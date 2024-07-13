dd91f3ca0f993369ef50710c7b32c284f35ad247
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-13 23:01:17 +0530

    Time: 122 ms (19.44%), Space: 33.7 MB (8.73%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,88 +1,43 @@
 class Solution {
 public:
-    int fun1(string &s, int x, int y){
-        stack<char> stk;
-        int ans = 0;
-        for (auto ch : s){
-            if (stk.empty()) stk.push(ch);
-            else if (ch == 'b'){
-                if (!stk.empty() && stk.top() == 'a'){
-                    stk.pop();
-                    ans+=x;
-                }
-                else{
+    int calculateMaxGain(string &s, int abPoints, int baPoints) {
+        auto processStack = [](string &str, char first, char second, int points) {
+            stack<char> stk;
+            int totalPoints = 0;
+            for (char ch : str) {
+                if (stk.empty() || ch != second || stk.top() != first) {
                     stk.push(ch);
-                }
-            }
-            else{
-                stk.push(ch);
-            }
-        }
-        string temp;
-        while (!stk.empty()){
-            temp.push_back(stk.top()); stk.pop();
-        }
-        reverse(temp.begin(), temp.end());
-        for (auto ch : temp){
-            if (stk.empty()) stk.push(ch);
-            else if (ch == 'a'){
-                if (!stk.empty() && stk.top() == 'b'){
+                } else {
                     stk.pop();
-                    ans+=y;
-                }
-                else{
-                    stk.push(ch);
+                    totalPoints += points;
                 }
             }
-            else{
-                stk.push(ch);
+            string remainingChars;
+            while (!stk.empty()) {
+                remainingChars.push_back(stk.top());
+                stk.pop();
             }
+            reverse(remainingChars.begin(), remainingChars.end());
+            return make_pair(totalPoints, remainingChars);
+        };
+
+        int totalPoints = 0;
+        if (abPoints >= baPoints) {
+            auto [points, remaining] = processStack(s, 'a', 'b', abPoints);
+            totalPoints += points;
+            auto [finalPoints, _] = processStack(remaining, 'b', 'a', baPoints);
+            totalPoints += finalPoints;
+        } else {
+            auto [points, remaining] = processStack(s, 'b', 'a', baPoints);
+            totalPoints += points;
+            auto [finalPoints, _] = processStack(remaining, 'a', 'b', abPoints);
+            totalPoints += finalPoints;
         }
-        return ans;
+
+        return totalPoints;
     }
-    int fun2(string &s, int x, int y){
-        stack<char> stk;
-        int ans = 0;
-        for (auto ch : s){
-            if (stk.empty()) stk.push(ch);
-            else if (ch == 'a'){
-                if (!stk.empty() && stk.top() == 'b'){
-                    stk.pop();
-                    ans+=y;
-                }
-                else{
-                    stk.push(ch);
-                }
-            }
-            else{
-                stk.push(ch);
-            }
-        }
-        string temp;
-        while (!stk.empty()){
-            temp.push_back(stk.top()); stk.pop();
-        }
-        reverse(temp.begin(), temp.end());
-        for (auto ch : temp){
-            if (stk.empty()) stk.push(ch);
-            else if (ch == 'b'){
-                if (!stk.empty() && stk.top() == 'a'){
-                    stk.pop();
-                    ans+=x;
-                }else{
-                    stk.push(ch);
-                }
-            }
-            else{
-                stk.push(ch);
-            }
-        }
-        return ans;
-    }
+
     int maximumGain(string s, int x, int y) {
-        if (x >= y){
-            return fun1(s, x, y);
-        }
-        return fun2(s, x, y);
+        return calculateMaxGain(s, x, y);
     }
 };
