3981d004084a63675f498835da0921fbcb447c33
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-19 08:14:41 +0530

    Time: 81 ms (54.64%), Space: 65.8 MB (34.78%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& arr, int limit) {
-        multiset<int> st;
         int n = arr.size(), i = 0, j = 0, maxi = 1;
+        deque<int> maxq, minq;
         while (j<n){
-            st.insert(arr[j]);
-            while (abs(*st.begin() - *st.rbegin()) > limit && i<=j){
-                if (st.find(arr[i]) != st.end()) {
-                    st.erase(st.find(arr[i]));
-                }
+            while (!maxq.empty() && arr[j] > maxq.back()) maxq.pop_back();
+            while (!minq.empty() && arr[j] < minq.back()) minq.pop_back();
+            maxq.push_back(arr[j]); minq.push_back(arr[j]);  
+            
+            while (abs(maxq.front()-minq.front()) > limit && i<=j){
+                if (arr[i] == maxq.front()) maxq.pop_front();
+                if (arr[i] == minq.front()) minq.pop_front();
                 i++;
             }
             maxi = max(maxi, j-i+1);
