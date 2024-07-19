0c33dd6c09ec7343b1eab9c1b472e701d6a04642
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-19 06:50:03 +0530

    Time: 317 ms (22.77%), Space: 145 MB (10.03%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    bool countFunction(string& arr, unordered_map<char,int>&mp, int k){
+    int getMaxUnique(string& s){
+        unordered_map<char, int> mp;
+        for (char ch : s) mp[ch]++;
+        return (int)mp.size();
+    }
+    int checkFreq(unordered_map<char,int> mp, int k){
         for (auto it : mp){
             if (it.second < k) return false;
         }
         return true;
     }
-    int longestSubstring(string arr, int k) {
-        int n=arr.size(),maxi = 0;
-        for (int i=0; i<n; i++){
+    int longestSubstring(string s, int k) {
+        int n = s.size(), maxi = 0;
+        if (k == 1) return (int)s.size();
+        int maxUnique =  getMaxUnique(s);
+        for (int b=1; b<=maxUnique; b++){
+            int i=0,j=0;
             unordered_map<char,int> mp;
-            for (int j=i; j<n; j++){
-                mp[arr[j]]++;
-                if (countFunction(arr, mp, k)) maxi = max(maxi, j-i+1);
+            while (j < n){
+                mp[s[j]]++;
+                while (mp.size() > b){
+                    mp[s[i]]--;
+                    if (mp[s[i]] == 0) mp.erase(s[i]);
+                    i++;
+                }
+                if (checkFreq(mp,k)){
+                    maxi = max(maxi, j-i+1);
+                }
+                j++;
             }
-        }
+        }     
         return maxi;
     }
-
 };
