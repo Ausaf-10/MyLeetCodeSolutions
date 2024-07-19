bbf73ec3cb18f4a63df6624dc78fb67a13f93718
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-19 11:43:06 +0530

    Time: 101 ms (96.18%), Space: 81.2 MB (27.08%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        int n = arr.size();
+        for (int i=0; i<n; i++){
+            if (mp.find(arr[i]) != mp.end()){
+                if (abs(i-mp[arr[i]]) <= k) return true;
+            }
+            mp[arr[i]]=i;
+        }
+        return false;
+    }
+};
