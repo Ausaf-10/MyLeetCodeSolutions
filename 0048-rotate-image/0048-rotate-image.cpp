3e384e7d167139f72145b24bb1d07b5a23047a0f
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-22 21:01:01 +0530

    Time: 0 ms (100%), Space: 8.9 MB (15.61%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        for (int i=0; i<matrix.size(); i++){
-            for (int j=i+1; j<matrix.size();j++){
-                swap(matrix[j][i],matrix[i][j]);
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        vector<vector<int>> newMat(n, vector<int>(n,0));
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                newMat[i][j] = mat[n-j-1][i];
             }
         }
-        for (int i=0; i<matrix.size();i++){
-            reverse(matrix[i].begin(),matrix[i].end());
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                mat[i][j] = newMat[i][j];
+            }
         }
-        return ;
+        return;
     }
 };
