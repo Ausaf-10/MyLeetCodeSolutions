b55d04e8568e063349f1924a215bcdf5e2859f5c
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-23 20:55:44 +0530

    Time: 3 ms (78.15%), Space: 12.3 MB (40.51%) - LeetHub

--- 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.cpp
@@ -12,17 +12,16 @@
 class Solution {
 public:
     void solve(TreeNode* root, int mini, int maxi, int &diff){
-        if (mini == 1e9 && maxi == -1e9){
-            if (root->left) solve(root->left, min(mini, root->val), max(maxi, root->val), diff);
-            if (root->right) solve(root->right, min(mini, root->val), max(maxi, root->val), diff);
-            return ;
+        
+        if (maxi != -1e9 && mini != 1e9){
+            diff = max(diff, abs(root->val - mini));
+            diff = max(diff, abs(root->val - maxi));
         }
-        int dif1 = abs(root->val - maxi);
-        int dif2 = abs(root->val - mini);
-        diff = max(diff, max(dif1, dif2));
-        if (root->left) solve(root->left, min(mini, root->val), max(maxi, root->val), diff);
-        if (root->right) solve(root->right, min(mini, root->val), max(maxi, root->val), diff);
-        return ;
+
+        if (root->left) solve(root->left, min(mini, root->val), max(root->val, maxi), diff);
+        if (root->right) solve(root->right, min(mini, root->val), max(root->val, maxi), diff);
+
+        return;
     }
     int maxAncestorDiff(TreeNode* root) {
         int mini = 1e9, maxi = -1e9, ans = -1e9;
