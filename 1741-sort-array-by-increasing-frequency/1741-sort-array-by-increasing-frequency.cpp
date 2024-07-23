13f2fd4f453b6309d4995fff20b7de18397b880d
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-23 09:50:30 +0530

    Time: 10 ms (29.58%), Space: 15 MB (25.59%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,34 +1,24 @@
-class cmp{
-    public:
-        bool operator()(pair<int,int>& a, pair<int,int>& b){
-            if (a.first == b.first) return a.second < b.second;
-            return a.first > b.first;
-        }
-};
 class Solution {
 public:
-    vector<int> frequencySort(vector<int>& nums) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, cmp > minHeap;
-        unordered_map<int,int> mp;
-        for (auto it : nums) mp[it]++;
-
+    vector<int> frequencySort(vector<int>& arr) {
+        vector<pair<int,int>> pairs;
+        unordered_map<int, int> mp;
+        auto cmp=[&](pair<int,int> a, pair<int, int> b){
+            if (a.second == b.second) return a.first > b.first;
+            return a.second < b.second;
+        };
+        for (auto it : arr) mp[it]++;
         for (auto it : mp){
-            int freq = it.second , value = it.first;
-            minHeap.push({freq,value});
+            pairs.push_back({it.first, it.second});
         }
-
+        sort(pairs.begin(), pairs.end(), cmp);
         vector<int> ans;
-
-        while (!minHeap.empty()){
-            int node = minHeap.top().second, freq = minHeap.top().first;
-            minHeap.pop();
+        for (auto it : pairs){
+            int freq = it.second, no = it.first;
             for (int i=0; i<freq; i++){
-                ans.push_back(node);
+                ans.push_back(no);
             }
-
         }
-
         return ans;
-    
     }
 };
