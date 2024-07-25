2077cb5477bbd22eb752180c9ec6ba65e2d9295e
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-25 14:06:43 +0530

    Time: 106 ms (75.25%), Space: 69.2 MB (86.44%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-
-    void heapify(vector<int>& arr,int i,int n){
-        int largest = i;
-
-        int left = 2*i+1, right = 2*i+2;
-
-        if (left < n && arr[largest] < arr[left]) largest = left;
-
-        if (right < n && arr[largest] < arr[right]) largest = right;
-
-        if (largest!=i){
-            swap(arr[largest],arr[i]);
-            heapify(arr,largest,n);
+    int n;
+    void heapify(int i, vector<int>& arr, int n){
+        int largest = i, left = 2*i+1, right = 2*i+2;
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+        if (i!=largest){
+            swap(arr[i], arr[largest]);
+            heapify(largest, arr, n);
         }
-
-        return; 
-    }
-
-    void buildHeap(vector<int>& arr,int n){
-        for (int i=n/2-1; i>=0; i--){
-            heapify(arr,i,n);
-        }
-
-        return;
-    }
-
-    void heapSort(vector<int>& arr, int n){
-        for (int i=n-1; i>=0; i--){
-            swap(arr[0],arr[i]);
-            heapify(arr,0,i);
-        }
-
         return ;
     }
-
+    void buildHeap(vector<int>& arr){
+        for (int i=n/2-1; i>=0; i--){
+            heapify(i, arr, n);
+        }
+        return ;
+    }
+    void heapSort(vector<int>& arr){
+        for (int i=n-1; i>=1; i--){
+            swap(arr[i], arr[0]);
+            heapify(0, arr, i);
+        }
+        return ;
+    }
     vector<int> sortArray(vector<int>& arr) {
-        int n = arr.size();
-        buildHeap(arr,n);
-        heapSort(arr,n);
+        n = arr.size();
+        buildHeap(arr);
+        heapSort(arr);
         return arr;
     }
 };
