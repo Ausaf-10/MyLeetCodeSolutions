efad2894286c00f32304ad6d697158862be2bd2c
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-26 07:18:39 +0530

    Time: 38 ms (21.81%), Space: 16 MB (28.76%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // int n = edges.size();
-        vector<vector<int>> grid(n, vector<int>(n,1e8));
+        vector<vector<int>> dist(n, vector<int>(n,1e9));
         for (auto it : edges){
             int u = it[0], v = it[1], wt = it[2];
-            grid[u][v] = wt;
-            grid[v][u] = wt;
+            dist[u][v] = dist[v][u] = wt;            
         }
-
-        for (int i=0; i<n; i++) grid[i][i] = 0;
-
+        for (int i=0; i<n; i++) dist[i][i] = 0;
         for (int k=0; k<n; k++){
             for (int i=0; i<n; i++){
                 for (int j=0; j<n; j++){
-                    if (grid[i][k] == 1e8 || grid[k][j] == 1e8) continue;
-                    grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j]);
+                    if (i == j || dist[i][k] == 1e9 || dist[k][j] == 1e9) continue;
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
 
-        int cntMax = INT_MAX;
-        int ans = -1;
+        int minCities = 1e9, indx = -1;
         for (int i=0; i<n; i++){
             int cnt = 0;
             for (int j=0; j<n; j++){
-                if (grid[i][j] <= distanceThreshold){
+                if (i!=j && dist[i][j] <= distanceThreshold){
                     cnt++;
                 }
             }
-            if (cnt <= cntMax){
-                cntMax = cnt;
-                ans = i;
+            if (cnt <= minCities){
+                minCities = cnt;
+                indx = i;
             }
         }
-
-        return ans;
-
+        return indx;
     }
 };
