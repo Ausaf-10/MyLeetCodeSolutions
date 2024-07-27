b113e9f7e8a2b018d801ce15a447f3d049c0f018
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-27 17:21:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (21%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -7,22 +7,20 @@ public:
         stack<char> stk;
         int open=0,close=0;
         for (int i=0; i<s.size(); i++){
-            if (s[i] == '(') stk.push(s[i]);
+            if (s[i] == '('){
+                stk.push(s[i]);
+                open++;
+            }
             else{
                 if (!stk.empty()){
-                    if (stk.top() == '(') stk.pop();
-                    else close++;
+                    stk.pop();
+                    open--;
                 }
                 else close++;
             }
         }
         if (stk.empty() && close == 0 && open == 0) return 0;
 
-        while (!stk.empty()){
-            open++;
-            stk.pop();
-            
-        }
         return open + close;
     }
 };
