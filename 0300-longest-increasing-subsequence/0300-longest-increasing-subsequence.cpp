2244e174c662c1980a17ab4f992db850a9bdf70d
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-07-29 20:52:31 +0530

    Time: 4 ms (95.12%), Space: 12.9 MB (67.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,32 +1,47 @@
 class Solution {
-public:
+public: 
     int n;
-    int recursion(vector<int>& arr, int indx, int prevIndx){
+    int recursion(int indx, int prevIndx, vector<int>& arr){
         if (indx == n) return 0;
-        int include = -1e9;
+        int pick = -1e9;
         if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
-            include = 1 + recursion(arr, indx+1, indx);
+            pick = 1 + recursion(indx+1, indx, arr);
         }
-        int notInclude = 0 + recursion(arr, indx+1, prevIndx);
-        return max(include, notInclude);
+        int notPick = 0 + recursion(indx + 1, prevIndx, arr);
+        return max(pick, notPick);
     }
     int tabulation(vector<int>& arr){
-        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
-        for (int indx=n-1; indx>=0; indx--){
-            for (int prevIndx = -1; prevIndx<indx; prevIndx++){
-                int include = -1e9;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for (int indx = n-1; indx>=0; indx--){
+            for (int prevIndx = -1; prevIndx < indx; prevIndx++){
+                int pick = -1e9;
                 if (prevIndx == -1 || arr[indx] > arr[prevIndx]){
-                    include = 1 + dp[indx+1][indx+1];
+                    pick = 1 + dp[indx+1][indx+1];
                 }
-                int notInclude = 0 + dp[indx+1][prevIndx+1];
-                dp[indx][prevIndx+1] = max(include, notInclude);
+                int notPick = 0 + dp[indx + 1][prevIndx+1];
+                dp[indx][prevIndx+1] = max(pick, notPick);
             }
         }
         return dp[0][0];
     }
+    int binarySearch(vector<int>& arr){
+        int maxi = 1;
+        vector<int> temp;
+        temp.push_back(arr[0]);
+        for (int i=1; i<n; i++){
+            int lb = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            if (lb == temp.size()){
+                temp.push_back(arr[i]);
+            }
+            else temp[lb] = arr[i];
+            maxi = max(maxi, (int)temp.size());
+        }
+        return maxi;
+    }
     int lengthOfLIS(vector<int>& arr) {
-        n = arr.size();       
-        // return recursion(arr, 0, -1);
-        return tabulation(arr); 
+        n = arr.size();
+        // return recursion(0, -1, arr);
+        // return tabulation(arr);
+        return binarySearch(arr);
     }
 };
