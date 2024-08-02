fd1e639be2a0374ee7793998aca6cdbdb038d735
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-02 07:11:57 +0530

    Time: 3 ms (29.62%), Space: 7.7 MB (15.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    int memoization(int n, vector<int>& dp){
-        if (n <= 1) return 1;
-        if (dp[n]!=-1) return dp[n];
-        return dp[n] = memoization(n-1, dp) + memoization(n-2, dp);
+    
+    int recursion(int indx){
+        if (indx == 0) return 1;
+        int pick = 0;
+        if (indx > 1) pick = recursion(indx - 2);
+        int notPick = recursion(indx - 1);
+        return notPick + pick;
+    }
+
+    int memoization(int indx, vector<int>& dp){
+        if (indx <= 1) return 1;
+        int lt = memoization(indx-1, dp);
+        int rt = memoization(indx-2, dp);
+        return dp[indx] = lt+rt;
     }
     int tabulation(int n){
-        vector<int> dp(n+1,-1);
-        dp[0] = 1, dp[1] = 1;
-        for (int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        vector<int> dp(n+1, -1);
+        dp[0] = 1;
+        for (int indx=1; indx<=n; indx++){
+            int pick = 0;
+            if (indx > 1) pick = dp[indx - 2];
+            int notPick = dp[indx - 1];
+            dp[indx] = pick + notPick;
         }
         return dp[n];
     }
-    int constantSpace(int n){
-        int prev1 = 1, prev2 = 1;
+    int spaceOptimization(int n){
+        int p1 = 1, p2 = 1;
         for (int i=2; i<=n; i++){
-            int sum = prev1 + prev2;
-            prev2 = prev1, prev1 = sum;
+            int temp = p1 + p2;
+            p2 = p1, p1 = temp;
         }
-        return prev1;
+        return p1;
     }
     int climbStairs(int n) {
-        // vector<int> dp(n+1,-1);
-        // return memoization(n,dp);
-        return constantSpace(n);
+        // vector<int> dp(n+1, -1);
+        return tabulation(n);
     }
 };
