975e2c68976e67d21565195d7c197d0bd0627586
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-02 14:45:11 +0530

    Time: 3 ms (95.22%), Space: 16.8 MB (26.64%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,80 +1,28 @@
 class Solution {
 public:
-    int recursion(int indx, bool buy, int cap,vector<int>& prices){
-        if (indx == prices.size() ||  cap == 0) return 0;
-        int profit = 0;
-        if (buy){
-            profit = max(-prices[indx] + recursion(indx+1, !buy, cap,prices), 0 + recursion(indx+1, buy, cap, prices));
-        }
-        else{
-            profit = max(prices[indx] + recursion(indx+1, !buy, cap-1, prices), 0 + recursion(indx+1, buy, cap,prices));
-        }
-        return profit;
-    }
-
-    int memoization(int indx, bool buy, int cap,vector<int>& prices, vector<vector<vector<int>>>& dp){
-        if (indx == prices.size() ||  cap == 0) return 0;
-        if (dp[indx][buy][cap]!=-1) return dp[indx][buy][cap];
-        int profit = 0;
-        if (buy){
-            profit = max(-prices[indx] + memoization(indx+1, !buy, cap,prices,dp), 0 + memoization(indx+1, buy, cap, prices,dp));
-        }
-        else{
-            profit = max(prices[indx] + memoization(indx+1, !buy, cap-1, prices,dp), 0 + memoization(indx+1, buy, cap,prices,dp));
-        }
-        return dp[indx][buy][cap] = profit;
-    }
-
-    int tabulation(vector<int>& prices, int cap){
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(cap+1,0)));
-        
-        for (int i=n-1; i>=0; i--){
-            for (int j=0; j<2; j++){
-                for (int k=1; k<cap+1; k++){
-                    int profit = 0;
-                    if (j){
-                        profit = max(-prices[i] + dp[i+1][!j][k],0 + dp[i+1][j][k]);
+    int n;
+    int tabulation(vector<int>& prices, int k){
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        for (int indx=n-1; indx>=0; indx--){
+            for (int buy = 0; buy<=1; buy++){
+                for (int cnt = 0; cnt < k; cnt++){
+                    if (buy){
+                        int sell = prices[indx] + dp[indx+1][!buy][cnt+1];
+                        int notSell = 0 + dp[indx+1][buy][cnt];
+                        dp[indx][buy][cnt] = max(sell, notSell);
                     }
                     else{
-                        profit =max(prices[i]+dp[i+1][!j][k-1], 0 + dp[i+1][j][k]);
+                        int buyThisDay = -prices[indx] + dp[indx+1][!buy][cnt];
+                        int notBuyThisDay = 0 + dp[indx+1][buy][cnt];
+                        dp[indx][buy][cnt] = max(buyThisDay, notBuyThisDay);
                     }
-                    dp[i][j][k] = profit;
                 }
             }
         }
-
-        return dp[0][1][cap];
+        return dp[0][0][0];
     }
-
-    int spaceOptimzation(vector<int>& prices, int cap){
-        int n = prices.size();
-        vector<vector<int>> prev (2, vector<int>(cap+1,0));
-
-        for (int i=n-1; i>=0; i--){
-            vector<vector<int>> temp (2, vector<int>(cap+1,0));
-            for (int j=0; j<2; j++){
-                for (int k=1; k<cap+1; k++){
-                    int profit = 0;
-                    if (j){
-                        profit = max(-prices[i] + prev[!j][k],0 + prev[j][k]);
-                    }
-                    else{
-                        profit =max(prices[i]+prev[!j][k-1], 0 + prev[j][k]);
-                    }
-                    temp[j][k] = profit;
-                }
-            }
-            prev = temp;
-        }
-
-        return prev[1][cap];
-        
-    }
-
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1,-1)));
+        n = prices.size();
         return tabulation(prices, k);
     }
 };
