9fa54af370439d1b0e971bfc34afc3e78ad1afa7
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-02 11:26:22 +0530

    Time: 35 ms (59.75%), Space: 47.1 MB (37.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,75 +1,55 @@
 class Solution {
-public: 
-    int recursion(int indx, int target, vector<int>& coins){
-        // BASE CASE
+public:
+    int recursion(vector<int> &arr, int target, int indx){
         if (indx == 0){
-            if (target % coins[indx] == 0) return target / coins[indx];
+            if (target % arr[0] == 0) return target / arr[0];
             return 1e9;
-        }   
-        int notPick = 0 + recursion(indx-1,target, coins);
-        int pick = INT_MAX;
-        if (coins[indx]<=target) pick = 1 + recursion(indx,target - coins[indx], coins);
-        return min(pick,notPick);
-
+        }
+        int notPick = 0 + recursion(arr, target, indx-1);
+        int pick = 1e9;
+        if (arr[indx] <= target){
+            pick = 1 + recursion(arr, target-arr[indx], indx);
+        }
+        return min(notPick, pick);
     }
-
-    int memoization(int indx, int target, vector<int>& coins, vector<vector<int>>& dp){
+    int memoization(vector<int> &arr, int target, int indx, vector<vector<int>>& dp){
         if (indx == 0){
-            if (target % coins[0] == 0) return target / coins[indx];
+            if (target % arr[0] == 0) return target / arr[0];
             return 1e9;
-        }   
-
-        if (dp[indx][target] !=-1) return dp[indx][target];
-
-        int notPick = 0 + memoization(indx-1,target, coins, dp);
-        int pick = INT_MAX;
-        if (coins[indx]<=target) pick = 1 + memoization(indx,target - coins[indx], coins, dp);
-        return dp[indx][target] = min(pick,notPick);
+        }
+        if (dp[indx][target] != -1) return dp[indx][target];
+        int notPick = 0 + memoization(arr, target, indx-1, dp);
+        int pick = 1e9;
+        if (arr[indx] <= target){
+            pick = 1 + memoization(arr, target-arr[indx], indx, dp);
+        }
+        return dp[indx][target]=min(notPick, pick);
     }
-    
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        // vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
-        // int ans = memoization(n-1, amount, coins, dp);
-        // return ans == 1e9 ? -1 : ans;
-
-        // vector<vector<int>> dp(n, vector<int> (amount + 1, 1e9)); // dp[indx][amount];
-        // BASE CASE
-        // for (int i=0; i<=amount; i++){
-        //     if (i % coins[0] == 0) dp[0][i] = i / coins[0];
-        // }
-        // for (int indx=1; indx<n; indx++){
-        //     for (int target = 0; target <= amount; target++){
-        //         int notPick = 0 + dp[indx-1][target];
-                
-        //         int pick = INT_MAX;
-        //         if (coins[indx]<=target) pick = 1 + dp[indx][target-coins[indx]];
-
-        //         dp[indx][target] = min(pick,notPick);
-        //     }
-        // }
-        // int ans = dp[n-1][amount];
-        // return ans == 1e9 ? -1 : ans;
-
-        vector<int> prev(amount + 1, 1e9);
+    int tabulation(vector<int>& arr, int amount){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1,1e9));
         for (int i=0; i<=amount; i++){
-            if (i % coins[0] == 0) prev[i] = i / coins[0];
+            if (i % arr[0] == 0) dp[0][i] = i/arr[0];
         }
-        for (int indx=1; indx< n ; indx++){
-            vector<int> temp(amount + 1);
-            for (int target = 0; target <= amount; target++){
-                
-                int notPick = 0 + prev[target];
-                
+
+        for (int indx=1; indx<n; indx++){
+            for (int target=0; target<=amount; target++){
+                int notPick = 0 + dp[indx-1][target] ;
                 int pick = 1e9;
-                if (coins[indx] <= target) pick = 1 + temp[target-coins[indx]];
-
-                temp[target] = min(pick,notPick);
+                if (arr[indx] <= target){
+                    pick = 1 + dp[indx][target-arr[indx]] ;
+                }
+                dp[indx][target]=min(notPick, pick);
             }
-
-            prev = temp;
         }
-        int ans = prev[amount];
-        return ans == 1e9 ? -1 : ans;
+
+        return dp[n-1][amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+        // // int x = memoization(coins, amount, n-1, dp);
+        int x = tabulation(coins, amount);
+        return x == 1e9 ? -1 : x;
     }
 };
