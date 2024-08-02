6ff488274a92591c700e5cdc400117cc535a949b
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-02 12:05:28 +0530

    Time: 11 ms (54.76%), Space: 19.8 MB (60.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,73 +1,53 @@
 class Solution {
 public:
-    int recursion(int indx, int target, vector<int>& coins){
-        if (target == 0) return 1;
+    int n ;
+    int recursion(vector<int>& arr, int target, int indx){
         if (indx == 0){
-            return (target % coins[0] == 0);
-        }
-        int notPick = recursion(indx-1, target, coins);
-        int pick = 0;
-        if (coins[indx] <= target) pick = recursion(indx, target - coins[indx], coins);
-        return pick + notPick;
-    }
-
-    int memoization(int indx, int target, vector<int>& coins, vector<vector<int>>& dp){
-        if (target == 0) return 1;
-        if (indx == 0){
-            if (target % coins[0] == 0) return 1;
+            if (target % arr[0] == 0) return 1;
             return 0;
         }
-        
-        if (dp[indx][target]!=-1) return dp[indx][target];
-        
-        int notPick = memoization(indx-1, target, coins, dp);
+        int notPick = recursion(arr, target, indx-1);
         int pick = 0;
-        if (coins[indx] <= target) pick = memoization(indx, target - coins[indx], coins, dp);
+        if (arr[indx] <= target){
+            pick = recursion(arr, target-arr[indx], indx);
+        }
+        return pick + notPick;
+    }
+    int memoization(vector<int>& arr, int target, int indx, vector<vector<int>>& dp){
+        if (indx == 0){
+            if (target % arr[0] == 0) return 1;
+            return 0;
+        }
+        if (dp[indx][target] != -1) return dp[indx][target];
+        int notPick = memoization(arr, target, indx-1, dp);
+        int pick = 0;
+        if (arr[indx] <= target){
+            pick = memoization(arr, target-arr[indx], indx, dp);
+        }
         return dp[indx][target] = pick + notPick;
     }
-
-    int tabulation(int amount, vector<int>& coins){
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,0));
-        for (int i=0; i<n; i++) dp[i][0] = 1;
-        for (int j=0; j<=amount; j++){
-            dp[0][j] = j % coins[0] == 0;
+    int tabulation(int k, vector<int>& arr){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(k+1,0));
+        for (int target=0; target<=k; target++){
+            if (target%arr[0] == 0) dp[0][target] = 1;
         }
-
-        for (int indx=1; indx<n; indx++){
-            for (int target=1; target<=amount; target++){
+        for (int indx = 1; indx<n; indx++){
+            for (int target=0; target<=k; target++){
                 int notPick = dp[indx-1][target];
                 int pick = 0;
-                if (coins[indx] <= target) pick = dp[indx][target - coins[indx]] ;
+                if (arr[indx] <= target){
+                    pick = dp[indx][target-arr[indx]];
+                }
                 dp[indx][target] = pick + notPick;
             }
         }
-        return dp[n-1][amount];
+        return dp[n-1][k];
     }
-
-    int spaceOptimization(int amount, vector<int>& coins){
-        int n = coins.size();
-        vector<int> prev(amount + 1, 0);
-        prev[0] = 1;
-        for (int j=1; j<=amount; j++) prev[j] = j%coins[0] == 0;
-
-        for (int indx=1; indx<n; indx++){
-            vector<int> temp(amount+1,0);
-            temp[0] = 1;
-            for (int target = 1; target<=amount; target++){
-                int notPick = prev[target];
-                int pick = 0;
-                if (coins[indx] <= target) pick = temp[target - coins[indx]] ;
-                temp[target] = pick + notPick;
-            }
-            prev = temp;
-        }
-        return prev[amount];
-    }
-
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return spaceOptimization(amount, coins);
+    int change(int target, vector<int>& arr) {
+        n = arr.size();
+        if (target == 0) return 1;
+        // vector<vector<int>> dp(n, vector<int>(target+1,
+        return tabulation(target, arr);
     }
 };
