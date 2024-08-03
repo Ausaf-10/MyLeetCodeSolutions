60845d8349e164e65aaf1ae7012f6827aecd3381
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-03 19:55:51 +0530

    Time: 39 ms (26.1%), Space: 25.8 MB (50.3%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,76 +1,40 @@
 class Solution {
 public:
-    int recursion(int indx1 , int indx2, string str1, string str2){
-        if (indx1 < 0 || indx2 < 0) return 0;
-        // MATCH
-        if (str1[indx1] == str2[indx2])
-            return 1 + recursion(indx1 - 1, indx2 - 1, str1, str2);
-        // NOT MATCH
-        return 0 + max(recursion(indx1, indx2 - 1, str1, str2), recursion(indx1 - 1, indx2, str1, str2));
-    }
-
-    int memoization(int indx1 , int indx2, string str1, string str2, vector<vector<int>>& dp){
+    int n,m;
+    int recursion(string s1, string s2, int indx1, int indx2){
         if (indx1 == 0 || indx2 == 0) return 0;
-        
-        if (dp[indx1][indx2] != -1) return dp[indx1][indx2];
-
-        // MATCH
-        if (str1[indx1-1] == str2[indx2-1])
-            return dp[indx1][indx2] = 1 + memoization(indx1 - 1, indx2 - 1, str1, str2, dp);
-        // NOT MATCH
-        return dp[indx1][indx2] = 0 + max(memoization(indx1, indx2 - 1, str1, str2, dp), memoization(indx1 - 1, indx2, str1, str2, dp));
+        int pick = -1e9, notPick = -1e9;
+        if (s1[indx1-1] == s2[indx2-1]){
+            pick = 1 + recursion(s1, s2, indx1-1, indx2-1);
+        }
+        else{
+            int one = 0 + recursion(s1, s2, indx1-1, indx2);
+            int two = 0 + recursion(s1, s2, indx1, indx2-1);
+            notPick = max(one, two);
+        }
+        return max(pick, notPick);
     }
-
-    int tabulation(string str1, string str2){
-        int n = str1.size();
-        int m = str2.size();
-        
-        vector<vector<int>> dp(n+1 , vector<int>(m+1,0));
-        // BASE CASE
-        for (int j=0; j<=m; j++ ) dp[0][j] = 0;
-        for (int i=0; i<=n; i++ ) dp[i][0] = 0;
-
-        for (int indx1 = 1; indx1 <= n; indx1++){
-            for (int indx2 = 1; indx2 <= m; indx2++){
-                if (str1[indx1 - 1] == str2[indx2 - 1]){
-                    dp[indx1][indx2] = 1 + dp[indx1 - 1][indx2 - 1];
+    int tabulation(string s1, string s2){
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for (int indx1=1; indx1<=n; indx1++){
+            for (int indx2=1; indx2<=m; indx2++){
+                int pick = -1e9, notPick = -1e9;
+                if (s1[indx1-1] == s2[indx2-1]){
+                    pick = 1 + dp[indx1-1][indx2-1];
                 }
-                else dp[indx1][indx2] =max(dp[indx1][indx2 - 1],dp[indx1 - 1][indx2]);
+                else{
+                    int one = 0 + dp[indx1-1][indx2];
+                    int two = 0 + dp[indx1][indx2-1];
+                    notPick = max(one, two);
+                }
+                dp[indx1][indx2] = max(pick, notPick);
             }
         }
         return dp[n][m];
     }
-
-    int spaceOptimization(string str1, string str2){
-        int n = str1.size();
-        int m = str2.size();
-
-        vector<int> prev(m+1,0);
-        for (int j = 0; j<=m; j++) prev[j] = 0;
-
-        for (int indx1 = 1; indx1 <= n; indx1++){
-            vector<int> temp(m+1,0);
-            temp[0] = 0;
-            for (int indx2 = 1; indx2 <= m; indx2++){
-                if (str1[indx1 - 1] == str2[indx2 - 1]){
-                    temp[indx2] = 1 + prev[indx2 - 1];
-                }
-                else temp[indx2] =max(temp[indx2 - 1],prev[indx2]);
-            }
-
-            prev = temp;
-        }
-
-        return prev[m];
-    }
-
-    int longestCommonSubsequence(string text1, string text2) {
-        // int n = text1.size();
-        // int m = text2.size();
-        
-        // vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
-
-        // return tabulation(text1, text2);
-        return spaceOptimization(text1, text2);
+    int longestCommonSubsequence(string s1, string s2) {
+        n = s1.size(), m = s2.size();
+        // return recursion(s1, s2, n, m);
+        return tabulation(s1, s2);
     }
 };
