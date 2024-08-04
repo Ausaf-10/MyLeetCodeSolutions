2e6507b1583491e50fea4c93e92c57f0f6aa9dbe
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-04 19:47:03 +0530

    Time: 16 ms (69.17%), Space: 20.1 MB (56.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,48 +1,47 @@
+using ll = long long int;
 class Solution {
 public:
-    int recursion(int indx1, int indx2,string str1, string str2){
-        if (indx2 < 0) return 1;
-        if (indx1 < 0) return 0;
-        if (str1[indx1] == str2[indx2]){
-            return recursion(indx1-1, indx2, str1, str2) + recursion(indx1-1, indx2-1, str1, str2);
-        }
-        return recursion(indx1-1, indx2, str1, str2);
-    }
-
-    int memoization(int indx1, int indx2,string str1, string str2, vector<vector<int>>& dp){
+    int n, m;
+    int recursion(int indx1, int indx2, string s, string t){
         if (indx2 == 0) return 1;
         if (indx1 == 0) return 0;
-        if (dp[indx1][indx2]!=-1) return dp[indx1][indx2];
-        
-        if (str1[indx1-1] == str2[indx2-1]){
-            return dp[indx1][indx2] = memoization(indx1-1, indx2, str1, str2, dp) + memoization(indx1-1, indx2-1, str1, str2, dp);
+
+        int one = 0, two = 0, three = 0;
+        if (s[indx1-1] == t[indx2-1]){
+            one = recursion(indx1-1, indx2-1, s, t);
+            two = recursion(indx1-1, indx2, s, t);
+        }  
+        else{
+            three = recursion(indx1-1, indx2, s, t);
         }
-        return dp[indx1][indx2] = memoization(indx1-1, indx2, str1, str2, dp);
+
+        return one + two + three;
     }
-
-    int tabulation(string str1, string str2){
-        int n = str1.size(), m = str2.size();
-        vector<vector<long long>> dp(n+1, vector<long long>(m+1,0));
-        // BASE CASE
-        for (int j=1; j<=m; j++) dp[0][j] = 0;
-        for (int i=0; i<=n; i++) dp[i][0] = 1;
-
-        for (int indx1 = 1; indx1<=n; indx1++){
-            for (int indx2 = 1; indx2<=m; indx2++){
-                if (str1[indx1-1] == str2[indx2-1]){
-                    dp[indx1][indx2] = (dp[indx1-1][indx2] + dp[indx1-1][indx2-1])%(int)(1e9+7);
-                    // dp[indx1][indx2]+=dp[indx1-1][indx2-1];
+    int tabulation(string s, string t){
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        for (int i=0; i<n; i++) dp[i][0] = 1;
+        for (int indx1=1; indx1<=n; indx1++){
+            for (int indx2=1; indx2<=m; indx2++){
+                int one = 0, two = 0, three = 0;
+                if (s[indx1-1] == t[indx2-1]){
+                    one = dp[indx1-1][indx2-1];
+                    two = dp[indx1-1][indx2];
+                }  
+                else{
+                    three = dp[indx1-1][indx2];
                 }
-            
-                else dp[indx1][indx2] =dp[indx1-1][indx2];
+                ll s = 0;
+                s = one;
+                s = s + two;
+                s = s + three;
+                dp[indx1][indx2] = s;
             }
-            
         }
-
         return dp[n][m];
     }
-
     int numDistinct(string s, string t) {
+        n = s.size(), m = t.size();
+        // return recursion(n, m, s, t);
         return tabulation(s, t);
     }
 };
