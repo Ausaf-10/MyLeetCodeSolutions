052f48e6d9febbc54d7939294b4634bdd87e2d63
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-06 11:52:09 +0530

    Time: 0 ms (100%), Space: 8.8 MB (23.46%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    
-    void find(int i,int n,int target,int k, vector<int> &ds,vector<vector<int>> &ans){
-        if (i > n || i>10) return;
-        
-        if (ds.size() == k){
-            
-            if (target == 0){
-                ans.push_back(ds);
+    int n,k;
+    void recursion(int indx, int var_sum, vector<int>& ds, vector<vector<int>>& ans){
+        if (ds.size() == k || indx == 10){
+            if (var_sum == 0){
+                if (ds.size() == k){
+                    ans.push_back(ds);
+                }
             }
-        
-            return;
+            return ;
         }
-        //PICK
-        if ( i <= target){
-            ds.push_back(i);
-            find(i+1,n,target-i,k,ds,ans);
-            ds.pop_back();
-        }
-        find(i+1,n,target,k,ds,ans);
-        return;
+        // PICK
+        ds.push_back(indx);
+        recursion(indx+1, var_sum-indx, ds, ans);
+        ds.pop_back();
+
+        // NOT PICK;
+        recursion(indx+1, var_sum, ds, ans);
+        return ;
     }
-    
-    vector<vector<int>> combinationSum3(int k, int n) {
-        // vector<int> nums = {1,2,3,4,5,6,7,8,9};
+    vector<vector<int>> combinationSum3(int K, int N) {
+        n = N, k = K;
         vector<int> ds; 
         vector<vector<int>> ans;
-        find(1,n,n,k,ds,ans);
+        recursion(1, n, ds, ans);
         return ans;
     }
 };
