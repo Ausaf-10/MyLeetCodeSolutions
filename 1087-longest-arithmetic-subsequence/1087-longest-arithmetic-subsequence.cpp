51e374b0ff23b0a841ce14008d13003094867c11
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-09 15:48:57 +0530

    Time: 108 ms (76.25%), Space: 144.4 MB (69.33%) - LeetHub

--- 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
@@ -1,47 +1,17 @@
-//Approach-1 : Recursion + Memoization (TLE)
 class Solution {
 public:
     int n;
-    int t[1001][1003];
-    
-    int solve(vector<int>& nums, int i, int diff) {
-        
-        if(i < 0)
-            return 0;
-        
-        if(t[i][diff+501] != -1)
-            return t[i][diff+501];
-        
-        int ans = 0;
-        
-        for(int k = i-1; k >= 0; k--) {
-            
-            if(nums[i] - nums[k] == diff)
-                ans = max(ans, 1 + solve(nums, k, diff));
-            
-        }
-        
-        return t[i][diff+501] = ans;
-    }
-    
-    int longestArithSeqLength(vector<int>& nums) {
-        n = nums.size();
-        if(n <= 2)
-            return n;
-        
-        memset(t, -1, sizeof(t));
-        
-        
-        int result = 0;
-        
-        for(int i = 0; i<n; i++) {
-            for(int j = i+1; j < n; j++) {
-                
-                result = max(result, 2 + solve(nums, i, nums[j] - nums[i]));
-                
+    int longestArithSeqLength(vector<int>& arr) {
+        n = arr.size();
+        int maxi = 0;
+        vector<vector<int>> dp(n, vector<int>(1001, 0));
+        for (int indx=1; indx<n; indx++){
+            for (int j=0; j<indx; j++){
+                int diff = arr[indx] - arr[j] + 500;
+                dp[indx][diff] = dp[j][diff] > 0 ? dp[j][diff] + 1 : 2;
+                maxi = max(maxi, dp[indx][diff]);
             }
         }
-        
-        return result;
+        return maxi;
     }
 };
