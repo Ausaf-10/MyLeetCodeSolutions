2da7b3550275a6df09689bb646991581db3c8e44
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-09 18:04:08 +0530

    Time: 34 ms (88.25%), Space: 26.5 MB (66.07%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public: 
-    bool compare(vector<int>& a, vector<int>& b){
-        if (a[1] < b[0]) return true;
-        return false;
+    static bool cmp(vector<int> &a, vector<int> &b){
+        return a[1] < b[1];
     }
     int findLongestChain(vector<vector<int>>& pairs) {
-        // int n = arr.size(), maxi = 1;
-        // auto cmp = [&](vector<int> a, vector<int> b){
-        //     return a[1] < b[1];
-        // };
-        // sort(arr.begin(), arr.end(),cmp);
-        // vector<int> dp(n,1);    
-        // for(int i=1; i<n; i++){
-        //     for(int prev_index = 0; prev_index <i; prev_index ++){
-        //         if( compare(arr[prev_index], arr[i]) && 1 + dp[prev_index] > dp[i]){
-        //             dp[i] = 1 + dp[prev_index];
-        //         }
-        //     }
-        //     maxi = max(maxi, dp[i]);
-        // }
-        // return maxi;
-        sort(pairs.begin(), pairs.end(), [](vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
-        });
 
-        vector<int> dp;
-        for (auto& pair : pairs) {
-            auto it = lower_bound(dp.begin(), dp.end(), pair[0]);
-            if (it == dp.end()) {
-                dp.push_back(pair[1]);
-            } else if (pair[1] < *it) {
-                *it = pair[1];
+        sort(pairs.begin(), pairs.end(), cmp);
+        int curr_end = pairs[0][1];
+
+
+        int cnt = 1;
+        for(int i = 1; i < pairs.size(); i++){
+            if(pairs[i][0] > curr_end){
+                cnt++;
+                curr_end = pairs[i][1];
+                
             }
         }
-        return dp.size();
+
+
+        return cnt;
+        
     }
 };
