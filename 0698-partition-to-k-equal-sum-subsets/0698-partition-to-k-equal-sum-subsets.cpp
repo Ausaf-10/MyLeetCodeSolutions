48275a844b71a29482124bb466ebbc06bac74ee1
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-10 13:26:32 +0530

    Time: 2005 ms (14.96%), Space: 11.3 MB (61.96%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -5,23 +5,21 @@ public:
         if (var_sum == 0) return recursion(0, reqSum, buckets+1, arr, vis);
         if (buckets == k) return true;
         if (indx >= n) return false;
-        if (vis[indx]) return recursion(indx+1, var_sum, buckets, arr, vis);
-        else{
-            bool op1 = false;
-            if (arr[indx] <= var_sum){
-                vis[indx] = 1;
-                op1 = recursion(indx+1, var_sum - arr[indx], buckets, arr, vis);
-                vis[indx] = 0;
-            }
-            bool op2 = recursion(indx+1, var_sum, buckets, arr, vis);
-            return op1 || op2; 
+        for (int i=indx; i<n; i++){
+            if (vis[i]) continue;
+            if (arr[i] > var_sum) break;
+            vis[i] = 1;
+            if (recursion(i+1, var_sum - arr[i], buckets, arr, vis)) return true;
+            vis[i] = 0;
         }
+        return false;
     }
     bool canPartitionKSubsets(vector<int>& arr, int K) {
         int totSum = accumulate(arr.begin(), arr.end(), 0);
         n = arr.size(), k = K, reqSum = totSum / k;
         if (totSum % k != 0) return false;
         vector<int> vis(n, 0);
+        sort(arr.begin(), arr.end());
         return recursion(0, reqSum, 0, arr, vis);
     }
 };
