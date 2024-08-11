5f041047c16b8d367bff8c6f615127d2e6c0e3bf
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-11 20:05:01 +0530

    Time: 3 ms (51.85%), Space: 9.8 MB (9.26%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -33,10 +33,31 @@ public:
             return dp[indx][open] = option1 || option2 || option3;
         }
     }
+
+    bool tabulation(string& s){
+        vector<vector<int>> dp(n+1, vector<int>(n+2, 0));
+        dp[n][0] = 1;
+        for (int indx=n-1; indx>=0; indx--){
+            for (int open=0; open<=n; open++){
+                if (s[indx] == '(') dp[indx][open] = dp[indx+1][open+1] ;
+                else if (s[indx] == ')'){
+                    if (open == 0) dp[indx][open] = false;
+                    else dp[indx][open] = dp[indx+1][open-1];
+                }
+                else{
+                    bool option1 = dp[indx+1][open] ;       // empty character
+                    bool option2 = dp[indx+1][open+1];    //  open 
+                    bool option3 = false;                          // close
+                    if (open > 0) option3 = dp[indx+1][open-1];
+                    dp[indx][open] = option1 || option2 || option3;
+                }
+            }
+        }
+        return dp[0][0];
+    }
     
     bool checkValidString(string s) {
         n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return memoization(0, 0, s, dp);
+        return tabulation(s);
     }
 };
