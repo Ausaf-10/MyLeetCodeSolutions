ea8ce6592f09645871e14a5edcb47016cb204f09
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-11 20:46:31 +0530

    Time: 708 ms (6.1%), Space: 54.2 MB (5.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
     int n;
-    bool solve(int indx, vector<int>& arr){
+    bool recursion(int indx, vector<int>& arr){
         if (indx == n-1) return true;
-        if (arr[indx] == 0) return false;
         for (int i=1; i<=arr[indx]; i++){
-            if (solve(indx+i, arr)) return true;
+            if (indx + i < n && recursion(indx+i, arr)) return true;
         }
         return false;
     }
-    bool memoization(int indx, vector<int>& arr, vector<int>& dp){
+    bool memoization(int indx, vector<int>& arr, vector<int> &dp){
         if (indx == n-1) return true;
-        if (arr[indx] == 0) return false;
         if (dp[indx]!=-1) return dp[indx];
         for (int i=1; i<=arr[indx]; i++){
-            if (i+indx < n && memoization(indx+i, arr, dp)) return dp[indx] = true;
+            if (indx + i < n && memoization(indx+i, arr, dp)) return dp[indx] = true;
         }
         return dp[indx] = false;
-    }
-    bool tabulation(vector<int>& arr){
-        vector<bool> dp(n, 0);
-        dp[n-1] = 1;
-        for (int indx=n-2; indx>=0; indx--){
-            if(arr[indx] == 0) continue;
-            int flag = 0;
-            for (int i=1; i<=arr[indx]; i++){
-                if (indx+i < n && dp[indx+i]){
-                    dp[indx] = true;
-                    flag = 1;
-                    break;
-                }
-            }
-        }
-        return dp[0];
     }
     bool canJump(vector<int>& arr) {
         n = arr.size();
         vector<int> dp(n, -1);
-        // return solve(0, arr);
-        return tabulation(arr);
-        // return memoization(0, arr, dp);
+        return memoization(0, arr, dp);
     }
 };
