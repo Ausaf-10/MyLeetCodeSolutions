0a4712ec7fb9436a7b1046ed2bae812708f13903
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-23 06:07:14 +0530

    Time: 16 ms (9.22%), Space: 16.5 MB (40.82%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,38 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    void reverse(ListNode* s, ListNode*e){
+    void reverseList(ListNode* s, ListNode* e){
+        if (s == NULL || s->next == NULL) return ;
         ListNode *prev = NULL , *curr = s, *after = s->next;
         while (curr!=e){
             curr->next = prev;
-            prev = curr, curr = after , after = after->next;
-        }
+            prev = curr, curr = after , after = after->next;  
+        } 
         curr->next = prev;
         return ;
-
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (head == NULL || head->next == NULL || k == 1) return head;
-        ListNode *s = head, *e = head;
-        int cnt = 1;
-        while (cnt < k){
+        if (head == NULL || head->next == NULL || k==1) return head;
+        ListNode* s = head, *e = head;
+        int inc = k-1;
+        while (inc--){
             e = e->next;
-            if (e == NULL) return head;
-            cnt++;
+            if (!e) return s;
         }
-        ListNode* newHead = e->next;
-        reverse(s,e);
-        ListNode *nextHead = reverseKGroup(newHead, k);
-        s->next = nextHead;
+        ListNode* afterHead = e->next;
+        reverseList(s, e);
+        ListNode* node = reverseKGroup(afterHead, k);
+        s->next = node;
         return e;
     }
 };
