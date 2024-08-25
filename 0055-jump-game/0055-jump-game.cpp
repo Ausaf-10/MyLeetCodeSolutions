18522926b91735232880205a5719a13928710ded
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-25 08:08:22 +0530

    Time: 2399 ms (5.01%), Space: 53.3 MB (8.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,8 +3,8 @@ public:
     int n;
     bool recursion(int indx, vector<int>& arr){
         if (indx == n-1) return true;
-        for (int i=1; i<=arr[indx]; i++){
-            if (indx + i < n && recursion(indx+i, arr)) return true;
+        for (int i=indx+1; i <= indx + arr[indx]; i++){
+            if (i < n && recursion(i, arr)) return true;
         }
         return false;
     }
@@ -19,15 +19,17 @@ public:
     bool tabulation(vector<int>& arr){
         vector<int> dp(n, 0);
         dp[n-1] = 1;
-        for (int indx = n-2; indx>=0; indx--){
-            for (int i=1; i<=arr[indx]; i++){
-                if (indx + i < n && dp[indx+i]) dp[indx] = true;
+        for (int indx=n-2; indx>=0; indx--){
+            if (arr[indx] == 0) continue;
+            for (int i=indx+1; i <= indx + arr[indx]; i++){
+                if (i < n && dp[i]) dp[indx] = true;
             }
         }
         return dp[0];
     }
     bool canJump(vector<int>& arr) {
         n = arr.size();
+        // return recursion(0, arr);
         return tabulation(arr);
     }
 };
