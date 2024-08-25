531d4876b45d8cc8ad96d30e1cf38cb81e581db5
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-25 09:35:56 +0530

    Time: 7 ms (87.92%), Space: 16.5 MB (8.88%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,27 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    void reverseList(ListNode* s, ListNode* e){
-        if (s == NULL || s->next == NULL) return ;
-        ListNode *prev = NULL , *curr = s, *after = s->next;
+    void reverse(ListNode* s, ListNode* e){
+        ListNode* prev = NULL, *curr = s, *after = s->next;
         while (curr!=e){
             curr->next = prev;
-            prev = curr, curr = after , after = after->next;  
+            prev = curr, curr = after, after = after->next;
         } 
         curr->next = prev;
         return ;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (head == NULL || head->next == NULL || k==1) return head;
-        ListNode* s = head, *e = head;
-        int inc = k-1;
-        while (inc--){
-            e = e->next;
-            if (!e) return s;
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode *e = head, *before = dummy;
+        int i = 0;
+        while (e!=NULL){
+            i++;
+            if (i%k == 0){
+                ListNode *after = e->next, *s = before->next;
+                reverse(s, e);
+                before->next = e, s->next = after;
+                e = after, before = s;
+            }
+            else{
+                e = e->next;
+            }
         }
-        ListNode* afterHead = e->next;
-        reverseList(s, e);
-        ListNode* node = reverseKGroup(afterHead, k);
-        s->next = node;
-        return e;
+        return dummy->next; 
     }
 };
