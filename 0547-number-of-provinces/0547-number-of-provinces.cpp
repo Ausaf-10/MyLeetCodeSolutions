682733f3a633fd803fb7f4d66dab7714265757ac
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-25 21:23:47 +0530

    Time: 18 ms (80.03%), Space: 18.2 MB (88.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,52 +1,27 @@
-
-class DisjointSet{
-    public:
-        vector<int> parent,size;
-        DisjointSet(int n){
-            size.resize(n+1,1);
-           
-            parent.resize(n+1,0);
-            for (int i=0; i<=n; i++) parent[i] = i;
-        }
-
-        int findUltimateParent(int node){
-            if (parent[node] == node) return node;
-            return parent[node] = findUltimateParent(parent[node]);
-        }
-
-        void UnionBySize(int u, int v){
-            int ulp_u = findUltimateParent(u);
-            int ulp_v = findUltimateParent(v);
-            if (ulp_u == ulp_v) return ;
-            if (size[ulp_u] < size[ulp_v]){
-                size[ulp_v]+=size[ulp_u];
-                parent[ulp_u] = parent[ulp_v]; 
-            }
-            else{
-                size[ulp_u]+=size[ulp_v];
-                parent[ulp_v] = parent[ulp_u]; 
-            }
-        }
-};
-
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& adj) {
-        int V = adj.size();
-        DisjointSet ds(V);
+private:
+    void dfs(int node,vector<vector<int>>& isConnected,vector<int>&vis,int V){
+        vis[node] = 1;
         for (int i=0; i<V; i++){
-            for (int j=0; j<V; j++){
-                if (adj[i][j] == 1){
-                    ds.UnionBySize(i,j);
-                }
+            if (i!=node && isConnected[node][i] == 1 && !vis[i]){
+                dfs(i,isConnected,vis,V);
             }
         }
-        int cnt = 0 ;
+        return ;
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<int> vis(V,0);
+        int cnt=0;
         for (int i=0; i<V; i++){
-            if (ds.parent[i] == i) cnt++;
+            if (vis[i] == 0){
+                dfs(i,isConnected,vis,V);
+                cnt++;
+            }
         }
-        
+
         return cnt;
-        
+
     }
 };
