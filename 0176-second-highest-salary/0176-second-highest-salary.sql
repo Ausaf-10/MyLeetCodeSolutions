35a443c13fde9325ec44d238e4716812a40ec054
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-29 21:20:09 +0530

    Time: 1103 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,5 @@
-SELECT 
-    IFNULL(
-        (SELECT DISTINCT salary 
-         FROM Employee 
-         ORDER BY salary DESC 
-         LIMIT 1 OFFSET 1), 
-    NULL) AS SecondHighestSalary ;
+SELECT (SELECT DISTINCT salary 
+        FROM Employee 
+        ORDER BY salary DESC 
+        LIMIT 1 OFFSET 1
+) AS SecondHighestSalary ;
