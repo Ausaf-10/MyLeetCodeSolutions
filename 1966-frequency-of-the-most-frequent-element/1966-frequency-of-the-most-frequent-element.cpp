8e0cdefd8a382d0d5a996fc2694f6dfa4110d7f7
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2025-04-14 09:21:28 +0530

    Time: 29 ms (93.58%), Space: 105.9 MB (61.42%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,31 +1,18 @@
 using ll = long long int;
 class Solution {
 public:
-    int n;
-    bool fun(ll mid, ll indx, vector<ll> &preSum, vector<int> &arr, int k){
-        int len = indx-mid+1;
-        ll operations, newSum = arr[indx]*1ll*len ;
-        operations = mid != 0 ? newSum - (preSum[indx]-preSum[mid-1]) : newSum - preSum[indx];
-        if (operations <= k) return true;
-        return false;
-    }
     int maxFrequency(vector<int>& arr, int k) {
-        n = arr.size();
-        vector<ll> preSum(n,0);
+        int n = arr.size(), i = 0, j = 0, maxi = 0;
+        ll s = 0;
         sort(arr.begin(), arr.end());
-        preSum[0] = arr[0];
-        for (int i=1; i<n; i++){
-            preSum[i] =  preSum[i-1] + arr[i];
-        }
-        int maxi = 1;
-        for(ll i=1; i<n; i++){
-            int low = 0, high = i;
-            while (low <= high){
-                ll mid = low + (high-low)/2;
-                if (fun(mid, i, preSum, arr, k)) high = mid - 1;
-                else low = mid + 1;
+        while (j < n){
+            s = s + arr[j];
+            while (((j-i+1)*1ll*arr[j] - s) > k){
+                s = s - arr[i];
+                i++;
             }
-            maxi = max(maxi, int(i-low+1));
+            maxi = max(maxi, j-i+1);
+            j++;
         }
         return maxi;
     }
