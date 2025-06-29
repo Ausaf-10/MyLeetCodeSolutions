638c921ae4e9643f1acede1f80d5b298ad033c4c
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2025-06-29 18:18:51 +0530

    Time: 11 ms (15.45%), Space: 23.7 MB (80.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,22 @@
+
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n = arr.size();
         vector<vector<int>> ans;
-        int start = intervals[0][0], end = intervals[0][1];
+        sort(arr.begin(), arr.end());
+        int prevStart = arr[0][0], prevEnd = arr[0][1];
         for (int i=1; i<n; i++){
-            int urStart = intervals[i][0], urEnd = intervals[i][1];
-            if (urStart <= end){
-                start = min(start, urStart);
-                end = max(end, urEnd);
-            } 
+            int curStart = arr[i][0], curEnd = arr[i][1];
+            if (curStart <= prevEnd){
+                prevEnd = max(prevEnd, curEnd);
+            }
             else{
-                ans.push_back({start,end});
-                start = intervals[i][0];
-                end = intervals[i][1];
-            } 
-
+                ans.push_back({prevStart, prevEnd});
+                prevStart = curStart, prevEnd = curEnd;
+            }
         }
-        ans.push_back({start,end});
-        
+        ans.push_back({prevStart, prevEnd});
         return ans;
     }
 };
