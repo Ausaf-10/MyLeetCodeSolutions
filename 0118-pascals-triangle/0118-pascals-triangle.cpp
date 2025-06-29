936a2564f4b1581fdbc4d4fe93477519f7eb6f46
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2025-06-29 15:56:52 +0530

    Time: 2 ms (15.71%), Space: 9.6 MB (62.89%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,32 +1,22 @@
-using ll = long long int;
 class Solution {
 public:
-    int nCr(int n, int r){
-        int num = 1, deno = 1;
-        for (int i=0; i<r; i++){
-            num*=(n-i);
-            num/=(i+1);
-        }
-        return num;
-    }
-    void printRow(int n, vector<int>& arr){
-        arr.push_back(1);
+    vector<int> returnRow(int row){
+        vector<int> ans;
+        ans.push_back(1);
         int res = 1;
-        for (int i=0; i<n; i++){
-            res*=(n-i);
+        for (int i=0; i<row; i++){
+            res*=(row-i);
             res/=(i+1);
-            arr.push_back(res);
+            ans.push_back(res); 
         }
-        return ;
+        return ans;
     }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> mat;
-        mat.push_back({1});
-        for (int rows = 2; rows <= numRows; rows++){
-            vector<int> rowArray;
-            printRow(rows-1,rowArray);
-            mat.push_back(rowArray);
+        vector<vector<int>> ans;
+        ans.push_back({1});
+        for (int i=2; i<=numRows; i++){
+            ans.push_back(returnRow(i-1));
         }
-        return mat;
+        return ans;
     }
 };
