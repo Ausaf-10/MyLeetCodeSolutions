35e99e53c81bf3cbbda438ec863266968055e497
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2025-07-03 20:53:25 +0530

    Time: 297 ms (57.91%), Space: 10.7 MB (62.35%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int n,m;
+    vector<vector<int>> directions = {{-1,0},{0,1},{1,0},{0,-1}};
     bool solve(vector<vector<char>>& board, string& word, int indx, int i, int j, vector<vector<int>>& vis){
         if (indx == word.size()) return true;
         if (i >= n || j>=m || i < 0 || j < 0 || board[i][j] != word[indx] || vis[i][j]) return false;
 
         vis[i][j] = 1;
-        vector<int> row = {-1, 0, 1, 0};
-        vector<int> col = {0, 1, 0, -1};
         for (int dr=0; dr<4; dr++){
-            int nrow = row[dr] + i;
-            int ncol = col[dr] + j;
+            int nrow = directions[dr][0] + i;
+            int ncol = directions[dr][1] + j;
                 if (solve(board, word, indx+1, nrow, ncol, vis)) return true;
         }
         
@@ -19,9 +18,9 @@ public:
     }
     bool exist(vector<vector<char>>& board, string word) {
         n = board.size(), m = board[0].size();
+        vector<vector<int>> vis (n, vector<int>(m,0));
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
-                vector<vector<int>> vis (n, vector<int>(m,0));
                 if (board[i][j] == word[0]){
                     if (solve(board, word, 0, i, j, vis)) return true;
                 }
